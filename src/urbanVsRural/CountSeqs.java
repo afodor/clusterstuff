4f127eb6a747012e18e4ce3aa6429449689f567b
afodor <anthony.fodor@gmail.com>
2014-11-24 18:54:41 -0500

    count sequences

--- src/urbanVsRural/CountSeqs.java
+++ src/urbanVsRural/CountSeqs.java
@@ -1,9 +1,49 @@
 package urbanVsRural;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.List;
+
+import parsers.FastaSequence;
+import jdk.jfr.events.FileWriteEvent;
+
 public class CountSeqs
 {
+	
+	public static int getReadNumber(String s) throws Exception
+	{
+		int readNum = Integer.parseInt( s.charAt(s.indexOf("fq")-2) + "");
+		
+		if (readNum < 1 || readNum > 2)
+			throw new Exception("No");
+		
+		return readNum;
+		
+	}
+	
+	
 	public static void main(String[] args) throws Exception
 	{
+		BufferedWriter writer = new BufferedWriter(new FileWriter(new File( 
+				"/projects/afodor/ChinaSequences/")));
 		
+		File topDir = new File("/projects/afodor/ChinaSequences/rdpResults");
+		
+		String[] list = topDir.list();
+		
+		for(String s : list)
+		{
+			if( s.endsWith(".fasta"))
+			{
+				List<FastaSequence> fastaList= 
+						FastaSequence.readFastaFile(topDir.getAbsolutePath() + File.separator + 
+								s);
+				
+				writer.write(s + "\t" + getReadNumber(s) + "\t" + fastaList.size() + "\n");
+			}
+		}
+		
+		writer.flush();  writer.close();
 	}
 }
