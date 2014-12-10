0727cee014c41d248659174f2cf9ec1c5171ba66
afodor <anthony.fodor@gmail.com>
2014-12-10 14:24:10 -0500

    useful to add the file to generate the report on!

--- src/vanderbilt/MakeKrakenScripts.java
+++ src/vanderbilt/MakeKrakenScripts.java
@@ -64,7 +64,8 @@ public class MakeKrakenScripts
 			
 			// report goes to standard output
 			writer.write("projects/afodor_research/krakenInstall/kraken-mpa-report " + 
-					"--db /projects/afodor_research/krakenInstall/krakenStandardDB2 \n" );
+					"--db /projects/afodor_research/krakenInstall/krakenStandardDB2 " + 
+					 " /projects/afodor_research/vanderbilt/krakenOut/" + nextDir.getName() + "_" + topDir.getName() + "_krakenData.txt ");
 			
 			writer.write("/projects/afodor_research/krakenInstall/kraken --threads 15 " + 
 					"--db /projects/afodor_research/krakenInstall/krakenHumanDB/" + 
