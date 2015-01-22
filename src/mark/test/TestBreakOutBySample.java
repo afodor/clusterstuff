a9436e3d426240b5bd2f045b6a1025d3fb2b1c83
afodor <anthony.fodor@gmail.com>
2015-01-22 14:42:58 -0500

    towards test code of breaking up FASTA file...

--- src/mark/test/TestBreakOutBySample.java
+++ src/mark/test/TestBreakOutBySample.java
@@ -19,23 +19,59 @@ public class TestBreakOutBySample
 					+ File.separator + "mrg_j200p5_q20_take2_l25_r25.fna"))));
 		
 		String line = inFasta.readLine();
+		BufferedReader lastReader = null;
+		StringBuffer stringBuff= null;
+		String lastHeader = null;
 		
 		while( line != null)
 		{
 			if( line.startsWith(">"))
 			{
-				String name = new StringTokenizer(line.substring(1), "_").nextToken();
+				if( stringBuff!= null)
+				{
+					String brokenheader = lastReader.readLine();
+					
+					if( ! brokenheader.startsWith(lastHeader))
+						throw new Exception("Unexpected " + lastHeader + " " + brokenheader);
+					
+					String nextSeq = lastReader.readLine();
+					
+					if( ! stringBuff.toString().equals(nextSeq))
+						throw new Exception("Mismatch " + stringBuff+ " " + nextSeq);
+				}
 				
-				BufferedReader brokenOutFasta = map.get(name);
+				lastHeader = new StringTokenizer(line.substring(1), "_").nextToken();
+				
+				BufferedReader brokenOutFasta = map.get(lastHeader);
 				
 				if( brokenOutFasta == null)
 				{
 					brokenOutFasta = new BufferedReader(new FileReader(new File(BreakOutBySample.OUT_SEQUENCE_DIR
-							+File.separator + name)));
-					map.put(name, brokenOutFasta);
+							+File.separator + lastHeader)));
+					map.put(lastHeader, brokenOutFasta);
 				}
+				
+				lastReader = brokenOutFasta;
+				stringBuff = new StringBuffer();				
+				line = inFasta.readLine();
+			}
+			else while( line != null && ! line.startsWith(">"))
+			{
+				stringBuff.append(line.trim());
 			}
 		}
+		
+		for(String s : map.keySet())
+		{
+			BufferedReader reader = map.get(s);
+			
+			if ( reader.readLine() != null)
+				throw new Exception("Not at end of file " + s);
+		}
+		
+		System.out.println("Global pass");
+
 	}
+	
 	
 }
