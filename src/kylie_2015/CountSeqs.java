57dc08aa8f9a31d3aa92f5dbb3766a9f148c90b7
afodor <anthony.fodor@gmail.com>
2015-02-06 13:52:27 -0500

    count fastq sequences...

--- src/kylie_2015/CountSeqs.java
+++ src/kylie_2015/CountSeqs.java
@@ -1,11 +1,46 @@
 package kylie_2015;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+import java.util.zip.GZIPInputStream;
+
+import parsers.FastQ;
+
 public class CountSeqs
 {
-	
-	
-	public static void main(String[] args)
+	public static void main(String[] args) throws Exception
 	{
+		BufferedWriter writer = new BufferedWriter(new FileWriter(
+				new File("/projects/afodor_research/kylie_2015/counts.txt" )));
+		
+		writer.write("sample\tcount\n");
+		
+		for(String s : CreateRDPQSub.FASTQ_DIR.list())
+		{
+			System.out.println(s);
+			File fastQFile = new File( CreateRDPQSub.FASTA_DIR.getAbsolutePath() + File.separator 
+						+ s);
+			
+			BufferedReader reader =
+					new BufferedReader(new InputStreamReader( 
+							new GZIPInputStream( new FileInputStream( fastQFile))));
+			
+			long count =0;
+			
+			for(FastQ fastq = FastQ.readOneOrNull(reader); fastq != null; 
+					fastq = FastQ.readOneOrNull(reader))
+			{
+				count++;
+			}
+			
+			writer.write(s + "\t" + count + "\n");
+		}
+		
+		writer.flush();  writer.close();
 		
 	}
 }
