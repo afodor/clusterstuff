c47509d4c0343d1db236eecc693fb3d0189fbba4
afodor <anthony.fodor@gmail.com>
2015-03-31 18:19:03 -0400

    first attempt to kick off RDP jobs...

--- src/jenniferTest/AllFastQToFastA.java
+++ src/jenniferTest/AllFastQToFastA.java
@@ -1,25 +1,13 @@
 package jenniferTest;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.List;
 
 public class AllFastQToFastA
 {
-	public static void main(String[] args) throws Exception
-	{
-		File startDir = new File("/projects/afodor_research/jwellerTestRunRawData");
-		
-		List<File> list = new ArrayList<File>();
-		recurse(startDir, list);
-		
-		for( File f : list)
-		{
-			if (f.getName().endsWith("fastq.gz"))
-				System.out.println(f.getAbsolutePath());
-		}
-	}
-	
 	private static void recurse(File dir, List<File> list) throws Exception
 	{
 		for(String s : dir.list())
@@ -31,5 +19,55 @@ public class AllFastQToFastA
 			else
 				list.add(f);
 		}
+	}
+
+	public static void main(String[] args) throws Exception
+	{
+		File startDir = new File("/projects/afodor_research/jwellerTestRunRawData");
+		File rdpDir = new File("/projects/afodor_research/jwellerTest/rdpOutDir");
+		File runDir = new File("/projects/afodor_research/jwellerTest/runDir");
+		File fastaDir = new File("/projects/afodor_research/jwellerTest/fastaDir");
+		
+		BufferedWriter allSHWriter = new BufferedWriter(new FileWriter(new File(
+				runDir.getAbsolutePath() + File.separator + "runAll.sh")));
+		
+		List<File> list = new ArrayList<File>();
+		recurse(startDir, list);
+		
+		int index=1;
+		for( File f : list)
+		{
+			File shFile = new File(runDir.getAbsolutePath() + "run_" + index + ".sh");
+			index++;
+			allSHWriter.write("qsub -q \"viper\" " + shFile.getAbsolutePath() +  "\n"  );
+			
+			BufferedWriter writer = new BufferedWriter(new FileWriter(shFile));
+			
+			if (f.getName().endsWith("fastq.gz"))
+			{
+				System.out.println(f.getAbsolutePath());
+				
+				String newName = f.getName().replace("fastq.gz", "fasta.txt");
+				
+				File fastAFile = new File(fastaDir.getAbsolutePath() + File.separator + 
+						newName	);
+
+				writer.write("java -cp /users/afodor/gitInstall/clusterstuff/bin " + 
+						"parsers.FastQToFastA" + " " + f.getAbsolutePath() +
+							" " + fastAFile + "\n");
+
+				File rdpFile = new File(rdpDir.getAbsolutePath() + File.separator + 
+								newName.replace("fasta.txt", "")+ "_rdpOut.txt"	);
+				
+				writer.write("java -jar /users/afodor/rdp/rdp_classifier_2.10.1/dist/classifier.jar " + 
+						"-o "+ rdpFile.getAbsolutePath() + " -q " + fastAFile+ "\n" );
+				
+				writer.flush();  writer.close();
+
+			}
+		}
+		
+		allSHWriter.flush();  allSHWriter.close();
+
 	}
 }
