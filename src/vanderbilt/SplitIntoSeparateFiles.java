d86ff9e0e7ab77e90e433fa6624d1817262e1ce4
Anthony Fodor <anthony.fodor@gmail.com>
2015-05-25 07:35:10 -0400

    split fasta corrected...

--- src/vanderbilt/SplitIntoSeparateFiles.java
+++ src/vanderbilt/SplitIntoSeparateFiles.java
@@ -10,13 +10,13 @@ import parsers.FastaSequenceOneAtATime;
 
 public class SplitIntoSeparateFiles
 {
-	static final File outDir = new File( "/projects/afodor/vanderbilt/split16S");
+	static final File outDir = new File( "/projects/afodor/vanderbilt/split	16S");
 	
 	public static void main(String[] args) throws Exception
 	{
 		HashMap<String, BufferedWriter> fileMap = new HashMap<String, BufferedWriter>();
 		split("/projects/afodor/vanderbilt/VanderbiltSequences_Dec52014/MSHR1/seqs_all1.fna", fileMap, "all1");
-		split("/projects/afodor/vanderbilt/VanderbiltSequences_Dec52014/MSHR2/seqs_all2.fna", fileMap, "all2");
+		split("/projects/afodor/vanderbilt/VanderbiltSequences_Dec52014/MSHR2/seqs_all2-CORRECTED.fna", fileMap, "all2");
 		
 		for( BufferedWriter writer : fileMap.values())
 		{
