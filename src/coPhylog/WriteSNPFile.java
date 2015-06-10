5ae3d41b7ffb02f3df41c90ba274455fb2617601
afodor <anthony.fodor@gmail.com>
2015-06-09 21:07:22 -0400

    stub for snp

--- src/coPhylog/WriteSNPFile.java
+++ src/coPhylog/WriteSNPFile.java
@@ -1,11 +1,83 @@
 package coPhylog;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
 public class WriteSNPFile
 {
 	private static int MIN_NUMBER_READS = 5;
 	
 	public static void main(String[] args) throws Exception
 	{
+		if( args.length != 2)
+		{
+			System.out.println("Usage context1 context2 outFile");
+			System.exit(1);
+			
+			File outFile = new File(args[2]);
+			
+			if( outFile.exists())
+				throw new Exception(outFile.getAbsolutePath() + " already exists ");
+			
+			HashMap<Long, ContextCount> map1 = 
+					CoPhylogBinaryFileReader.readBinaryFileRequireMin(new File(args[0]), MIN_NUMBER_READS);
+			
+			HashMap<Long, ContextCount> map2 = 
+					CoPhylogBinaryFileReader.readBinaryFileRequireMin(new File(args[1]), MIN_NUMBER_READS);
+			
+			System.out.println("Comparing " + map1.size() + " " + map2.size());
+			
+			List<Holder> snpList = new ArrayList<WriteSNPFile.Holder>();
+			
+			for( Long l : map1.keySet() )
+			{
+				if( map2.containsKey(l))
+				{
+					ContextCount cc1 = map1.get(l);
+					ContextCount cc2 = map2.get(l);
+					
+					if( cc1.isDifferentInHighest(cc2))
+					{
+						Holder h = new Holder();
+						h.cc1 = cc1;
+						h.cc2 = cc2;
+						h.distance = cc1.getRawDistance(cc2);
+						snpList.add(h);
+					}
+				}
+			}
+				
+			Collections.sort(snpList);
+			System.out.println("Found " + snpList.size() + " out of " + map1.size() + " " + map2.size());
+			writeResults(outFile, snpList);
+		}
+	}
+	
+	private static void writeResults(File outFile, List<Holder> snpList) throws Exception
+	{
+
+		if( outFile.exists())
+			throw new Exception(outFile.getAbsolutePath() + " already exists ");
 		
+		BufferedWriter writer = new BufferedWriter(new FileWriter(outFile));
+		writer.flush();  writer.close();
+	}
+	
+	private static class Holder implements Comparable<Holder>
+	{
+		ContextCount cc1;
+		ContextCount cc2;
+		double distance;
+		
+		@Override
+		public int compareTo(Holder o)
+		{
+			return Double.compare(o.distance, this.distance);
+		}
 	}
 }
