037f1fcc15bfb59951159e11447f592d1114ddbc
kwinglee <kwinglee@uncc.edu>
2015-06-12 13:11:14 -0400

    Scripts to run cophylog on all CHS genomes

---
+++ src/chs_snps/WriteBinaryContextsFromFastQ.java
@@ -0,0 +1,115 @@
+package chs_snps;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
+
+import coPhylog.ContextCount;
+import coPhylog.ContextHash;
+import parsers.FastQ;
+import parsers.FastaSequence;
+import parsers.FastaSequenceOneAtATime;
+
+public class WriteBinaryContextsFromFastQ
+{
+	public static void main(String[] args) throws Exception
+	{
+		if( args.length != 2)
+		{
+			System.out.println("Usage inFile outFile");
+			System.exit(1);
+		}
+		
+		process(new File(args[0]), new File(args[1]));
+	}
+	
+	public static void process(File inFile, File outFile ) throws Exception
+	{
+		int numDone =0;
+		long lastTime = System.currentTimeMillis();
+		File logFile = new File(outFile.getAbsoluteFile() + "_LOG.txt");
+		BufferedWriter logWriter = new BufferedWriter(new FileWriter(logFile));
+		
+		logWriter.write("numDone\ttotalMemory\tfreeMemory\tmaxMemory\tfractionFree\tfractionAllocated\ttimeSinceLast\n");
+		
+		
+		int contextSize = 13;
+		
+		if(outFile.exists())
+			throw new Exception("Out file exists " + outFile.getAbsolutePath());
+		
+		BufferedReader reader = 
+		new BufferedReader(new InputStreamReader( 
+				new GZIPInputStream( new FileInputStream( inFile))));
+		
+		HashMap<Long, ContextCount> map = new HashMap<Long, ContextCount>();
+		
+		for( FastQ fq =  FastQ.readOneOrNull(reader); fq != null; 
+					fq = FastQ.readOneOrNull(reader))
+		{
+			ContextHash.addToHash(fq.getSequence(), map, contextSize);
+			numDone++;
+			
+			if( numDone % 10000 == 0 )
+			{
+				System.gc();
+
+				double fractionFree= 1- (Runtime.getRuntime().totalMemory()- ((double)Runtime.getRuntime().freeMemory() ))
+						/Runtime.getRuntime().totalMemory();
+
+				double fractionAllocated = 1-  (Runtime.getRuntime().maxMemory()- ((double)Runtime.getRuntime().totalMemory() ))
+						/Runtime.getRuntime().maxMemory();
+				
+				logWriter.write( numDone + "\t" + Runtime.getRuntime().totalMemory() + "\t" +
+						Runtime.getRuntime().freeMemory()  + "\t" + 
+						Runtime.getRuntime().maxMemory() + "\t" + fractionFree  + "\t" + 
+						fractionAllocated + "\t" + (System.currentTimeMillis() - lastTime) / 1000f  + "\n"
+								);
+				
+				lastTime = System.currentTimeMillis();
+				
+				logWriter.flush();
+
+			}
+		}
+		
+		if(outFile.exists())
+			throw new Exception("Out file exists " + outFile);
+		
+		writeBinaryFile(outFile, map);
+		logWriter.flush(); logWriter.close();
+	}
+	
+
+	private static void writeBinaryFile(File outFile, HashMap<Long, ContextCount> map ) throws Exception
+	{
+		DataOutputStream out =new DataOutputStream( new BufferedOutputStream(new GZIPOutputStream(new FileOutputStream(outFile))));
+		
+		out.writeInt(map.size());
+		
+		for( Long l : map.keySet() )
+		{
+			out.writeLong(l);
+			
+			ContextCount cc = map.get(l);
+			
+			out.writeByte( cc.getAAsByte() );
+			out.writeByte( cc.getCAsByte());
+			out.writeByte(cc.getGAsByte());
+			out.writeByte( cc.getTAsByte());
+		}
+		
+		out.flush();  out.close();
+		
+	}
+	
+}
---
+++ src/chs_snps/WriteScriptsForCreatingBinaryFilesAll80CHS.java
@@ -0,0 +1,47 @@
+package chs_snps;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+
+import utils.ConfigReader;
+
+
+public class WriteScriptsForCreatingBinaryFilesAll80CHS
+{
+	public static void main(String[] args) throws Exception
+	{
+		BufferedWriter writer = new BufferedWriter(new FileWriter(new File("/projects/afodor_chs/run/runAll.sh")));
+		
+		File folder = new File("/projects/afodor_research/mjzapata/CRE/CHS_raw/");
+		File[] files = folder.listFiles();
+		
+		for( int i=0; i < files.length; i++)
+		{
+			String inFile = files[i].getName();
+			if(inFile.endsWith(".fastq.gz")) {
+				inFile.replace(".fastq.gz", "");
+				File outFile = new File("/projects/afodor_chs/kwinglee/cophylog_all80chs/run/run_" + inFile);
+				
+				BufferedWriter aWriter = new BufferedWriter(new FileWriter(outFile));
+				
+				aWriter.write("#PBS -l nodes=1:ppn=8\n");
+				aWriter.write("#PBS -W x=NODESET:ONEOF:FEATURE:ib_ddr\n");
+	
+				//aWriter.write("#PBS -l nodes=1:ppn=16\n");
+				//aWriter.write("#PBS -W x=NODESET:ONEOF:FEATURE:ib_qdr2\n");
+				
+				writer.write("qsub -q \"viper\" run_" + inFile  + "\n");
+				
+				
+				aWriter.write("java -cp /users/afodor/gitInstall/clusterstuff/bin -mx20000m chs_snps.WriteBinaryContextsFromFastQ "
+						+ "/projects/afodor_research/mjzapata/CRE/CHS_raw/" + inFile + ".fastq.gz /projects/afodor_chs/kwinglee/cophylog_all80chs/context" + 
+							inFile + "_context.gz");
+				
+				aWriter.flush();  aWriter.close();
+			}
+		}
+		
+		writer.flush();  writer.close();
+	}
+}
