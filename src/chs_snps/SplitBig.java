87018fff009c17f1afbbd6e221a2808c18cca72b
kwinglee <kwinglee@uncc.edu>
2015-06-25 10:08:39 -0400

    split big files in 4

--- src/chs_snps/SplitBig.java
+++ src/chs_snps/SplitBig.java
@@ -28,7 +28,7 @@ public class SplitBig {
 		
 		//log file
 		BufferedWriter log = new BufferedWriter(new FileWriter(new File(outdir+"SplitBig_log")));
-		log.write("file\tNumLines1\tNumLines2\tTotalLines\n");
+		log.write("file\tNumLines1\tNumLines2\tNumLines3\tNumLines4TotalLines\n");
 		
 		//command to run scripts
 		BufferedWriter all = new BufferedWriter(new FileWriter(new File(outdir+"runAllContext.sh")));
@@ -38,6 +38,8 @@ public class SplitBig {
 			//set up scripts to run contexts
 			all.write("qsub -q \"Cobra_batch\" run_" + f  + "A\n");
 			all.write("qsub -q \"Cobra_batch\" run_" + f  + "B\n");
+			all.write("qsub -q \"Cobra_batch\" run_" + f  + "C\n");
+			all.write("qsub -q \"Cobra_batch\" run_" + f  + "D\n");
 			
 			BufferedWriter script = new BufferedWriter(new FileWriter(new File(outdir+"run_"+f+"A")));
 			script.write("#PBS -l nodes=1:ppn=12\n");
@@ -53,6 +55,20 @@ public class SplitBig {
 						f + "B_context.gz");
 			script.close();
 			
+			script = new BufferedWriter(new FileWriter(new File(outdir+"run_"+f+"C")));
+			script.write("#PBS -l nodes=1:ppn=12\n");
+			script.write("java -cp /users/kwinglee/git/clusterstuff/bin -mx30000m chs_snps.WriteBinaryContextsFromFastQ "
+					+ outdir + f + "C.fastq.gz " + outdir + "context" + 
+						f + "C_context.gz");
+			script.close();
+			
+			script = new BufferedWriter(new FileWriter(new File(outdir+"run_"+f+"D")));
+			script.write("#PBS -l nodes=1:ppn=12\n");
+			script.write("java -cp /users/kwinglee/git/clusterstuff/bin -mx30000m chs_snps.WriteBinaryContextsFromFastQ "
+					+ outdir + f + "D.fastq.gz " + outdir + "context" + 
+						f + "D_context.gz");
+			script.close();
+			
 			//split file
 			BufferedReader reader = 
 					new BufferedReader(new InputStreamReader( 
@@ -61,10 +77,16 @@ public class SplitBig {
 					new GZIPOutputStream(new FileOutputStream(new File(outdir+f+"A.fastq.gz")))));
 			BufferedWriter out2 = new BufferedWriter(new OutputStreamWriter(
 					new GZIPOutputStream(new FileOutputStream(new File(outdir+f+"B.fastq.gz")))));
+			BufferedWriter out3 = new BufferedWriter(new OutputStreamWriter(
+					new GZIPOutputStream(new FileOutputStream(new File(outdir+f+"C.fastq.gz")))));
+			BufferedWriter out4 = new BufferedWriter(new OutputStreamWriter(
+					new GZIPOutputStream(new FileOutputStream(new File(outdir+f+"D.fastq.gz")))));
 			String line = reader.readLine();
 			int count = 1; //number of lines read
 			int c1 = 0;//num reads in 1
 			int c2 = 0;//num read in 2
+			int c3 = 0;//num reads in 3
+			int c4 = 0;//num reads in 4
 			while(line != null) {
 				if(count <= 4) {
 					out1.write(line+"\n");
@@ -72,7 +94,11 @@ public class SplitBig {
 				} else if(count <= 8) {
 					out2.write(line + "\n");
 					c2++;
-					if(count == 8) {
+				} else if(count <=12) {
+					out3.write(line + "\n");
+				} else if(count <=16) {
+					out4.write(line+"\n");
+					if(count == 16) {
 						count = 0;
 					}
 				} else {
@@ -81,11 +107,13 @@ public class SplitBig {
 				count++;
 				line = reader.readLine();
 			}
-			log.write(f + "\t" + c1 + "\t" + c2 + "\t" + (c1+c2)+"\n");
+			log.write(f + "\t" + c1 + "\t" + c2 + "\t" + c3 + "\t" + c4 + "\t" + (c1+c2+c3+c4)+"\n");
 			log.flush();
 			reader.close();
 			out1.close();
 			out2.close();
+			out3.close();
+			out4.close();
 		}
 		log.close();
 		all.close();
