08026b5e6d5f933ee08b4cb2793073ddfb15e741
kwinglee <kwinglee@uncc.edu>
2015-07-07 11:52:35 -0400

    fix submission to cobra

--- src/chs_snps/WriteScriptsForComparePairsAll80CHS.java
+++ src/chs_snps/WriteScriptsForComparePairsAll80CHS.java
@@ -56,11 +56,11 @@ public class WriteScriptsForComparePairsAll80CHS
 						+ getCHS(yFile) + "_compare.txt");
 				
 				File outScriptFile = new File("/projects/afodor_research/kwinglee/cophylog_all80chs/runCompare/runC_" + index);
-				allWriter.write("#PBS -l nodes=1:ppn=12\n");
 				allWriter.write("qsub -q \"Cobra_batch\" " + outScriptFile.getName() +  "\n");
 				
 				BufferedWriter scriptWriter = new BufferedWriter(new FileWriter(outScriptFile));
-				scriptWriter.write("java -cp /users/kwinglee/git/clusterstuff/bin -mx30000m " + 
+				scriptWriter.write("#PBS -l nodes=1:ppn=12\n");
+				scriptWriter.write("java -cp /users/kwinglee/git/clusterstuff/bin -Xmx36g " + 
 							"coPhylog.WriteSNPFile " + xFile.getAbsolutePath()  + " " + yFile.getAbsolutePath() + " " + 
 							outFile.getAbsolutePath());
 				
