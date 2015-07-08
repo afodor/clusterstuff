784ffe8320e765edbbf621413d68cbd009130a29
kwinglee <kwinglee@uncc.edu>
2015-07-08 11:46:37 -0400

    more test code

--- src/chs_snps/Test.java
+++ src/chs_snps/Test.java
@@ -4,6 +4,7 @@ import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
@@ -21,7 +22,7 @@ public class Test {
 	
 	public static void main(String[] args) throws Exception {
 		if(args.length != 1) {
-			System.err.println("Usage: 1=write gzip file, 2=write unzipped file, 3=read gzip file, 4=read unzipped file");
+			System.err.println("Usage: 1=write gzip file, 2=write unzipped file, 3=read gzip file, 4=read unzipped file, 5=read context file");
 			System.exit(1);
 		}
 		String strain = "CHS01";
@@ -33,8 +34,10 @@ public class Test {
 			read(outDir + strain + "_context.gz");
 		} else if(args[0].equals("4")) {
 			read(outDir + strain + "_context_unzip.gz");
+		} else if(args[0].equals("5")) {
+			read("/projects/afodor_research/kwinglee/cophylog_all80chs/context/contextSRR1206030_2_context.gz");
 		} else {
-			System.err.println("Usage: 1=write gzip file, 2=write unzipped file, 3=read gzip file, 4=read unzipped file");
+			System.err.println("Usage: 1=write gzip file, 2=write unzipped file, 3=read gzip file, 4=read unzipped file, 5=read context file");
 			System.exit(1);
 		}
 	}
@@ -48,11 +51,13 @@ public class Test {
 			tot += cc.getSum();
 		}
 		System.out.println(tot);
+		System.out.println(map1.size());
 	}
 	
-	public static void writeZip(String strain) throws IOException {
+	public static HashMap<Long, ContextCount> getHash(String strain) throws IOException {
 		BufferedReader convert = new BufferedReader(new FileReader (new File(conversionFile)));
 		String line = convert.readLine();
+		HashMap<Long, ContextCount> map=null;
 		while(line != null) {
 			String[] csp = line.split("\t");
 			String chs;
@@ -63,7 +68,7 @@ public class Test {
 			}
 			if(chs.equals(strain)) {
 				//set up hash
-				HashMap<Long, ContextCount> map = new HashMap<Long, ContextCount>();
+				map = new HashMap<Long, ContextCount>();
 
 				//combine files into hash
 				String[] srrlist = csp[1].replace("[", "").replace("]", "").split(",");
@@ -92,112 +97,62 @@ public class Test {
 						} catch (Exception e) {
 							System.err.println(e);
 						}
-
-
-						//update log
-						System.gc();
 					}
 				}
-
-				//write results
-				DataOutputStream out =new DataOutputStream( new BufferedOutputStream(new GZIPOutputStream(new FileOutputStream(outDir + chs + "_context.gz"))));
-
-				out.writeInt(map.size());
-
-				for( Long l : map.keySet() ) {
-
-					ContextCount cc = map.get(l);
-					int sum = cc.getSum();
-					if(sum > MIN_READS) {
-						out.writeLong(l);
-						out.writeByte(cc.getAAsByte());
-						out.writeByte(cc.getCAsByte());
-						out.writeByte(cc.getGAsByte());
-						out.writeByte(cc.getTAsByte());
-					}
-				} 
-				out.close();
 			}
-			
 			line = convert.readLine();
-			
 		}
 		convert.close();
+		return(map);
+	}
+	
+	public static void writeZip(String strain) throws IOException {
+		HashMap<Long, ContextCount> map = getHash(strain);
+
+		//write results
+		DataOutputStream out =new DataOutputStream( new BufferedOutputStream(new GZIPOutputStream(new FileOutputStream(outDir + strain + "_context.gz"))));
+
+		out.writeInt(map.size());
+
+		for( Long l : map.keySet() ) {
+
+			ContextCount cc = map.get(l);
+			int sum = cc.getSum();
+			if(sum > MIN_READS) {
+				out.writeLong(l);
+				out.writeByte(cc.getAAsByte());
+				out.writeByte(cc.getCAsByte());
+				out.writeByte(cc.getGAsByte());
+				out.writeByte(cc.getTAsByte());
+			}
+		} 
+		out.flush(); 
+		out.close();
+			
 	}
 
 	public static void writeUnzip(String strain) throws IOException {
-		BufferedReader convert = new BufferedReader(new FileReader (new File(conversionFile)));
-		String line = convert.readLine();
-		while(line != null) {
-			String[] csp = line.split("\t");
-			String chs;
-			if(csp[0].length() == 1) {
-				chs = "CHS0" + csp[0];
-			} else {
-				chs = "CHS" + csp[0];
+		HashMap<Long, ContextCount> map = getHash(strain);
+		
+		//write results
+		DataOutputStream out =new DataOutputStream(new FileOutputStream(outDir + strain + "_context_unzip"));
+
+		out.writeInt(map.size());
+
+		for( Long l : map.keySet() ) {
+
+			ContextCount cc = map.get(l);
+			int sum = cc.getSum();
+			if(sum > MIN_READS) {
+				out.writeLong(l);
+				out.writeByte(cc.getAAsByte());
+				out.writeByte(cc.getCAsByte());
+				out.writeByte(cc.getGAsByte());
+				out.writeByte(cc.getTAsByte());
 			}
-			if(chs.equals(strain)) {
-				//set up hash
-				HashMap<Long, ContextCount> map = new HashMap<Long, ContextCount>();
-
-				//combine files into hash
-				String[] srrlist = csp[1].replace("[", "").replace("]", "").split(",");
-				for(int i = 0; i < srrlist.length; i+=2) {//read every other file, because each file is listed twice
-					for(int j = 1; j <= 2; j++) {//forward and reverse reads
-						try {
-							//read file
-							HashMap<Long, ContextCount> m = CoPhylogBinaryFileReader.readBinaryFileRequireMin(new File(contextDir+"context"+srrlist[i].trim()+"_"+j+"_context.gz"), 2);
-
-							System.out.println(srrlist[i].trim()+"_"+j);
-							
-							//merge maps
-							for(Long key : m.keySet()) {
-								ContextCount con = m.get(key);
-								if(map.containsKey(key)) {
-									ContextCount mapcon = map.get(key);
-									mapcon.increaseA(con.getNumA());
-									mapcon.increaseT(con.getNumT());
-									mapcon.increaseC(con.getNumC());
-									mapcon.increaseG(con.getNumG());
-									map.put(key, mapcon);
-								} else {
-									map.put(key, con);
-								}
-							}
-						} catch (Exception e) {
-							System.err.println(e);
-						}
-
-
-						//update log
-						System.gc();
-					}
-				}
-
-				//write results
-				DataOutputStream out =new DataOutputStream(new FileOutputStream(outDir + chs + "_context_unzip"));
-
-				out.writeInt(map.size());
-
-				for( Long l : map.keySet() ) {
-
-					ContextCount cc = map.get(l);
-					int sum = cc.getSum();
-					if(sum > MIN_READS) {
-						out.writeLong(l);
-						out.writeByte(cc.getAAsByte());
-						out.writeByte(cc.getCAsByte());
-						out.writeByte(cc.getGAsByte());
-						out.writeByte(cc.getTAsByte());
-					}
-				} 
-				out.close();
-			}
-			
-			line = convert.readLine();
-			
-		}
-		convert.close();
+		} 
+		out.flush(); 
+		out.close();
 	}
 
 }
