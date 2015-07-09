0fc7c01e39b9b2b6a62b9e84a1768ca60bb8dc52
afodor <anthony.fodor@gmail.com>
2015-07-09 10:06:01 -0400

    update file paths for second run...

--- src/jobinCardioRun1/Demultiplex.java
+++ src/jobinCardioRun1/Demultiplex.java
@@ -14,15 +14,15 @@ import parsers.FastQ;
 public class Demultiplex
 {
 	public static File forwardSeqs = new File(
-			"/projects/afodor_research/JobinCardio/*/Sample_1-26241228/Data/Intensities/BaseCalls/Sample-Name-1_S1_L001_R1_001.fastq.gz");
+			"/projects/afodor_research/JobinCardio/*/Sample_1-26241227/Data/Intensities/BaseCalls/Sample-Name-1_S1_L001_R1_001.fastq.gz");
 	
 	public static File reverseSeqs = new File(
-			"/projects/afodor_research/JobinCardio/*/Sample_1-26241228/Data/Intensities/BaseCalls/Sample-Name-1_S1_L001_R2_001.fastq.gz");
+			"/projects/afodor_research/JobinCardio/*/Sample_1-26241227/Data/Intensities/BaseCalls/Sample-Name-1_S1_L001_R2_001.fastq.gz");
 	
 	public static File keyFile = new File(
-			"/projects/afodor_research/JobinCardio/keys/barcode Run2 5-30-2015.txt");
+			"/projects/afodor_research/JobinCardio/keys/Barcode Run1 8-6-2015.txt");
 	
-	public static File fastaOutDir = new File("/projects/afodor_research/JobinCardio/fastaOut");
+	public static File fastaOutDir = new File("/projects/afodor_research/JobinCardio/fastaOutRun1");
 	
 	public static void main(String[] args) throws Exception
 	{
