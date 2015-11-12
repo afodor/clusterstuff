2391c363ad604ca90b2c25e7d6b551f005924271
kwinglee <kwinglee@uncc.edu>
2015-11-12 15:58:25 -0500

    code to convert China wgs to fasta

---
+++ src/kw_china_wgs/fastqToFasta.java
@@ -0,0 +1,68 @@
+/**
+ * convert the fastq files from CPC to fasta
+ * (input to shotmap must be in .fa or .fa.gz format)
+ */
+package kw_china_wgs;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
+
+public class fastqToFasta implements Runnable {
+	private File fName;
+	public fastqToFasta(File name) {
+		this.fName = name;
+	}
+	public void run() {
+		String outDir = "/projects/afodor_research/kwinglee/china/wgs/fastas/";//directory to write fastas to
+		String name = this.fName.getName().replace(".fq.gz", "");
+		BufferedReader fq;
+		try {
+			fq = new BufferedReader(new InputStreamReader(new GZIPInputStream( new FileInputStream(fName))));
+			BufferedWriter fa = new BufferedWriter(new OutputStreamWriter(new GZIPOutputStream(new FileOutputStream(new File(outDir + name + ".fa.gz")))));
+			String line1 = fq.readLine();//seq id
+			while(line1 != null) {
+				String line2 = fq.readLine();//seqeunce
+				fq.readLine();//+
+				fq.readLine();//quality scores
+				String id = line1.replaceFirst("@", ">");
+				fa.write(id + "\n");
+				fa.write(line2 + "\n");
+				line1 = fq.readLine();
+			}
+			fq.close();
+			fa.close();
+		} catch (Exception e) {
+			System.out.println("error in " + name);
+			e.printStackTrace();
+		} 
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		String fqDir = "/projects/afodor_research/kwinglee/china/wgs/from_cpc/0.cleandata/";//directory containing fastq sequences from CPC
+		
+		File inDir = new File(fqDir);
+		File[] fqList = inDir.listFiles();
+		Thread[] allThreads = new Thread[fqList.length];
+		for(int i = 0; i < fqList.length; i++) {
+			if(fqList[i].getName().endsWith(".fq.gz")) {
+				Runnable r = new fastqToFasta(fqList[i]);
+				Thread t = new Thread(r);
+				t.start();	
+				allThreads[i] = t;
+			}
+		}
+		
+		//make sure all threads finish
+		for(int i = 0; i < allThreads.length; i++) {
+			allThreads[i].join();
+		}
+	}
+
+}
---
+++ src/kw_china_wgs/testFastqToFasta.java
@@ -0,0 +1,64 @@
+/**
+ * Test that got all reads in fastq to fasta conversion
+ */
+package kw_china_wgs;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.zip.GZIPInputStream;
+
+public class testFastqToFasta {
+	
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		String fqDir = "/projects/afodor_research/kwinglee/china/wgs/from_cpc/0.cleandata/";//directory containing fastq sequences from CPC
+		String faDir = "/projects/afodor_research/kwinglee/china/wgs/fastas/";//directory to write fastas to
+		
+		//set up output
+		BufferedWriter out = new BufferedWriter(new FileWriter(new File("/projects/afodor_research/kwinglee/china/wgs/fastaCheck.txt")));
+		out.write("numLines_fastq\tnumLines_fasta\tpass\n");
+		
+		File inDir = new File(fqDir);
+		File[] fqList = inDir.listFiles();
+		for(int i = 0; i < fqList.length; i++) {
+			if(fqList[i].getName().endsWith(".fq.gz")) {
+				//fastq count
+				String name = fqList[i].getName().replace(".fq.gz", "");
+				BufferedReader fq = new BufferedReader(new InputStreamReader(new GZIPInputStream( new FileInputStream(fqList[i]))));
+				int numLinesFq = 0;
+				String line = fq.readLine();
+				while(line != null) {
+					numLinesFq++;
+					line = fq.readLine();
+				}
+				fq.close();
+				
+				//fasta count
+				BufferedReader fa = new BufferedReader(new InputStreamReader(new GZIPInputStream( new FileInputStream(new File(faDir + name + ".fa.gz")))));
+				int numLinesFa = 0;
+				line = fa.readLine();
+				while(line != null) {
+					numLinesFa++;
+					line = fa.readLine();
+				}
+				fa.close();
+				
+				//write results
+				out.write(numLinesFq + "\t" + numLinesFa + "\t");
+				if(numLinesFq == numLinesFa * 2) {
+					out.write("pass\n");
+				} else {
+					out.write("FAIL\n");
+					System.out.println(name);
+				}
+			}
+		}
+		out.close();
+	}
+
+}
