12b2378e036fc906493860b09161f66ca39a8644
afodor <anthony.fodor@gmail.com>
2015-11-23 16:07:30 -0500

    attempt to gather

--- src/creOrthologs/Gather.java
+++ src/creOrthologs/Gather.java
@@ -3,6 +3,11 @@ package creOrthologs;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 
 import parsers.HitScores;
 
@@ -10,22 +15,86 @@ public class Gather
 {
 	public static void main(String[] args) throws Exception
 	{
-		BufferedWriter writer = new BufferedWriter(new FileWriter(
-				new File("/projects/afodor_research/af_broad/blastResults.txt")));
+		HashMap<String, Float> countMap = getCountMap();
+		writeFile(countMap);
+	}
 	
-		writer.write( "filepath\tquery\ttarget\tgenomeFileName\tmaxBitScore\n");
+	private static List<String> getVals(HashMap<String, Float> counts ,boolean first)
+			throws Exception
+		{
+			HashSet<String> set= new HashSet<String>();
+			
+			for(String s : counts.keySet())
+			{
+				String[] splits = s.split("@");
+				
+				if( splits.length != 2  )
+					throw new Exception("No");
+				
+				if( first)
+					set.add(splits[0]);
+				else
+					set.add(splits[1]);
+			}
+			
+			List<String> list = new ArrayList<String>(set);
+			Collections.sort(list);
+			return list;
+		}
+	
+	private static void writeFile(HashMap<String, Float> counts) throws Exception
+	{
+		System.out.println("Writing file");
+		List<String> orthologKeys = getVals(counts, true);
 		
+		List<String> genomes = getVals(counts, false);
+		
+		BufferedWriter writer = new BufferedWriter(new FileWriter(new File(
+				"/projects/afodor_research/af_broad/bitScoreOrthologsAsColumns.txt")));
+		
+		writer.write("genome");
+		
+		for(String s : orthologKeys)
+			writer.write("\tLine_" + s);
+		
+		writer.write("\n");
+		
+		for(String g : genomes)
+		{
+			writer.write(g);
+			
+			for(String s : orthologKeys)
+			{
+				Float val = counts.get(s + "@" + g);
+				
+				if( val == null)
+					val = 0f;
+				
+				writer.write("\t" + val);
+			}
+			
+			writer.write("\n");
+		}
+		
+		writer.flush(); writer.close();
+	}
+	
+	
+	private static HashMap<String, Float> getCountMap() throws Exception
+	{
+		HashMap<String, Float> countMap = getCountMap();
+		HashMap<String, HashSet<Integer>> orthoMap = DefaultTabParser.getFileLineMap();
+	
 		long numDone =0;
 		long numFound = 0;
 		
 		BufferedWriter logWriter =new BufferedWriter(new FileWriter(new File( 
-				"/projects/afodor_research/af_broad/log.txt")));
+				"/projects/afodor_research/af_broad/logOrthoPivot.txt")));
 		
 		String[] innerList = MakeBlastDB.ORTHOLOG_DIR.list();
 		
 		for(String d : RunBlastAll.DIRECTORIES)
 		{
-			
 			File genomeDir = new File("/projects/afodor_research/af_broad" + File.separator + d);
 			
 			String[] list = genomeDir.list();
@@ -38,30 +107,50 @@ public class Gather
 					
 					for(String s2 : innerList)
 					{
-						if( s2.endsWith("dnaseq"))
+						if( s2.endsWith("geneseq"))
 						{
 							File outFile = new File( outSubDir.getAbsolutePath()+ File.separator + 
 									s.replaceAll(".scaffolds.fasta","") + "_" + d + "_to_" + 
-									s2.replaceAll("dnaseq", "") + "txt.gz");
+									s2.replaceAll("geneseq", "") + "txt.gz");
 							
 							if( outFile.exists())
 							{
 								numFound++;
 								
-								HitScores hs = HitScores.getTopHitByBitScore(outFile);
+								List<HitScores> hsList = HitScores.getAsList(outFile,true);
 								
-								if( hs != null )
+								for( HitScores hs : hsList )
 								{
-									writer.write( outFile.getAbsolutePath() + "\t" + 
-													hs.getQueryId() + "\t" + hs.getTargetId() + "\t" + 
-											s.replaceAll(".scaffolds.fasta","") + "\t" + hs.getBitScore() + "\n"
-												);
+									String genome = hs.getQueryId();
+									String target = hs.getTargetId();
+									
+									if( genome.indexOf("@") != -1 ) 
+										throw new Exception("No " + genome);
+									
+									HashSet<Integer> lineIDs = orthoMap.get(target);
+									
+									if( lineIDs != null)
+									{
+										for(Integer i : lineIDs)
+										{
+											String key = i +"@" + genome;
+											
+											Float old = countMap.get(key);
+											
+											if( old == null || hs.getBitScore() > old)
+												countMap.put(key,hs.getBitScore());
+										}
+									}
+									else
+									{
+										logWriter.write("Could not find cluster " + target );
+									}
 								}
 								
 							}
 							else
 							{
-								//logWriter.write("Could not find " + outFile.getAbsoluteFile() + "\n");
+								logWriter.write("Could not find file " + outFile.getAbsoluteFile() + "\n");
 							}
 								
 							numDone++;
@@ -79,6 +168,6 @@ public class Gather
 		}
 		
 		logWriter.flush();  logWriter.close();
-		writer.flush();  writer.close();
+		return countMap;
 	}
 }
