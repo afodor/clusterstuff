802017620d3a44f4c43e69d082cee6766c5e8b57
afodor <anthony.fodor@gmail.com>
2015-11-25 20:28:10 -0500

    towards pulling out the relevant sequences...

---
+++ src/creOrthologs/ExtractAlignment.java
@@ -0,0 +1,38 @@
+package creOrthologs;
+
+import java.io.File;
+
+public class ExtractAlignment
+{
+	public static void main(String[] args) throws Exception
+	{
+		File topDir = new File( "/projects/afodor_research/af_broad/individualBlastRuns/contig_7000000220927531");
+		
+		String[] list = topDir.list();
+		
+		for( String s : list)
+		{
+			String[] splits = s.split("_");
+			String aName = splits[2] + "_" + splits[3] + "_" + splits[4] + "_" + splits[5] + "scaffolds.fasta";
+			File aFile = findFile(aName);
+			System.out.println(aFile.getAbsolutePath());
+			
+		}
+		
+		
+	}
+	
+	private static File findFile(String genome) throws Exception
+	{
+		for(String d : RunBlastAll.DIRECTORIES)
+		{
+			File genomeDir = new File("/projects/afodor_research/af_broad" + File.separator + genome);
+			
+			if(genomeDir.exists())
+				return genomeDir;
+		}	
+		
+		
+		throw new Exception("Could not find " + genome);
+	}
+}
