2fb2ed1a63402d06d0b2cb7678d91007ef0d838d
Anthony Fodor <anthony.fodor@gmail.com>
2016-01-07 11:16:07 -0500

    test to extract one automatically...

---
+++ src/creOrthologs/automatedDistanceMatrix/GenerateDistanceMatrix.java
@@ -0,0 +1,66 @@
+package creOrthologs.automatedDistanceMatrix;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.util.List;
+
+import parsers.FastaSequence;
+
+public class GenerateDistanceMatrix
+{
+	private static final File WORKING_DIR = new File("/nobackup/afodor_research/af_broad/workingDir");
+	
+	public static void main(String[] args) throws Exception
+	{
+		if(args.length != 5)
+		{
+			System.out.println("Usage inputGenomeFilePath contigName startPos endPos outDistanceMatrix");
+			System.exit(1);
+		}
+		
+		File topDir = new File(WORKING_DIR.getAbsolutePath() + File.separator + 
+						args[0] +"_" + args[1] + "_" + args[2] + "_" + args[3] );
+		
+		topDir.mkdir();
+		writeQuerySequence(args, topDir);
+	}
+	
+	private static File writeQuerySequence( String[] args , File topDir )
+		throws Exception
+	{
+		List<FastaSequence> list = 
+				FastaSequence.readFastaFile(args[0]);
+		
+		String toFind = args[1];
+		
+		File outFile = 
+				new File(
+						topDir.getAbsolutePath() + File.separator + 
+						args[0] +"_" + args[1] + "_" + args[2] + "_" + args[3] + ".fasta");
+		
+		boolean foundOne = false;
+			
+		for(FastaSequence fs : list)
+		{
+			if( !foundOne && fs.getFirstTokenOfHeader().equals(toFind))
+			{
+				BufferedWriter writer = new BufferedWriter(new FileWriter(outFile));
+				
+				writer.write(fs.getHeader() + "\n");
+				writer.write(fs.getSequence().substring(Integer.parseInt(args[2]), 
+						Integer.parseInt(args[3])) + "\n");
+				
+				foundOne = true;
+
+				writer.flush();  writer.close();
+			}
+		}
+			
+		if( foundOne )
+			throw new Exception("Could not find query sequence " + 
+					args[0] +"_" + args[1] + "_" + args[2] + "_" + args[3] );
+		
+		return outFile;
+	}		
+}
---
+++ src/creOrthologs/automatedDistanceMatrix/ScriptsForMultipleQueries.java
@@ -0,0 +1,46 @@
+package creOrthologs.automatedDistanceMatrix;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+
+public class ScriptsForMultipleQueries
+{
+	private static File SCRIPT_DIR = 
+		new File("/nobackup/afodor_research/af_broad/scripts/runQueriesToDistance");
+	
+	private static File OUTPUT_DIR = 
+			new File("/nobackup/afodor_research/af_broad/distanceMatrices");
+	
+	private static File INPUT_GENOME = 
+			new File("/projects/afodor_research/af_broad/carolina/klebsiella_pneumoniae_chs_11.0.scaffolds.fasta");
+	
+	public static void main(String[] args) throws Exception
+	{
+		writeOneFile(INPUT_GENOME.getAbsolutePath(), "contig_7000000220927533", 729729, 749719);
+	}
+	
+	private static File writeOneFile( String genomePath, String contig, 
+										int startPos, int endPos)  throws Exception
+	{
+		File shFile = new File(SCRIPT_DIR + File.separator + 
+				genomePath + "_" + contig + "_" + startPos + "_"+ endPos + ".sh");
+		
+		File outFile = new File(OUTPUT_DIR + File.separator +
+				genomePath + "_" + contig + "_" + startPos + "_"+ endPos + "_dist.txt");
+		
+		BufferedWriter writer = new BufferedWriter(new FileWriter(shFile));
+		
+		
+		writer.write(
+				"java -jar " + 
+		"/users/afodor/gitInstall/clusterstuff/bin/creOrthologs.automatedDistanceMatrix.GenerateDistanceMatrix " 
+		+ genomePath + " " + contig + " " + startPos + " " + endPos + " " + 
+					outFile.getAbsolutePath() +  "\n" );
+				
+		writer.flush(); writer.close();
+		
+		return shFile;
+	}
+	
+}
