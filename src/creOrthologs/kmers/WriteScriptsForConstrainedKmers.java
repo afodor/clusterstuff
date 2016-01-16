90388ac5d9e2bf0a84af708614b3f413d97c7431
afodor <anthony.fodor@gmail.com>
2016-01-16 09:53:43 -0500

    stub for running all constrained k-mers...

---
+++ src/creOrthologs/kmers/WriteScriptsForConstrainedKmers.java
@@ -0,0 +1,19 @@
+package creOrthologs.kmers;
+
+public class WriteScriptsForConstrainedKmers
+{
+	public static void main(String[] args) throws Exception
+	{
+		String genomePath = 
+				"/nobackup/afodor_research/af_broad/carolina/klebsiella_pneumoniae_chs_11.0.scaffolds.fasta";
+		
+		String contig = "7000000220927538";
+		
+		
+		
+		for( int x=0; x < 300000; x++)
+		{
+			
+		}
+	}
+}
