8ea4bf1c90ef0943e2566168999a6a0483f29d98
kwinglee <kwinglee@uncc.edu>
2016-01-25 12:25:54 -0500

    make QIIME map

---
+++ src/kw_jobinAnaerobe/makeQiimeMap.java
@@ -0,0 +1,27 @@
+/**
+ * generate mapping file for QIIME
+ */
+package kw_jobinAnaerobe;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class makeQiimeMap {
+	public static String DIR = "/nobackup/afodor_research/kwinglee/jobin/anaerobe/";
+	
+	public static void main(String[] args) throws IOException {
+		File[] fastas = new File(DIR + "join").listFiles();
+		BufferedWriter out = new BufferedWriter(new FileWriter(new File("DIR" + "qiimeMap.txt")));
+		out.write("#SampleID\tBarcodeSequence\tLinkerPrimerSequence\tInputFileName\tDescription\n");
+		for(File f : fastas) {
+			String name = f.getName();
+			if(name.endsWith("join.fastq")) {
+				String id = name.replaceAll("join.fastq", "").replaceAll("anaerobe_", "");
+				out.write(id + "\tX\tX\t" + name + "\n");
+			}
+		}
+		out.close();
+	}
+}
