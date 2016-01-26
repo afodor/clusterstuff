e52c67637ebb3bcaed5564681c229a6075405aa0
kwinglee <kwinglee@uncc.edu>
2016-01-26 10:39:08 -0500

    get info on CHS11 genes for graphing

---
+++ src/kw_rbh/GenomePositions.java
@@ -0,0 +1,68 @@
+/**
+ * For the given genome, print a table of scaffold, start and stop for each gene
+ * Also get sizes of each scaffold
+ * For graphs of ortholog p-values
+ */
+package kw_rbh;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class GenomePositions {
+	public static String GenomeDir = "/nobackup/afodor_research/af_broad/";
+	public static String OutDir = "/nobackup/afodor_research/kwinglee/cre/rbh/";
+	public static String Group = "carolina";
+	public static String Genome = "klebsiella_pneumoniae_chs_11.0";//CHS11
+	
+	public static void main(String[] args) throws IOException {
+		//get table of gene positions
+		BufferedReader gtf = new BufferedReader(new FileReader(new File(
+				GenomeDir+Genome+".genes.gtf")));
+		BufferedWriter out = new BufferedWriter(new FileWriter(new File(
+				OutDir + Group + "_" + Genome + "_genePositions.txt")));
+		out.write("geneID\tscaffold\tgeneStart\tgeneStop\n");
+		String line = gtf.readLine();
+		while(line != null) {
+			String[] sp = line.split("\t");
+			if(sp[2].equals("exon")) { //only include exons (which include start and stop codon)
+				//use gene_id
+				String id = sp[8].split(";")[0].split("\"")[1];
+				out.write(Group + "_" + Genome + "_" + id + "\t"
+						+ sp[0] + "\t" + sp[3] + "\t" + sp[4] + "\n");
+			}
+			line = gtf.readLine();
+		}
+		
+		gtf.close();
+		out.close();
+		
+		//get scaffold sizes
+		BufferedReader fasta = new BufferedReader(new FileReader(new File(
+				GenomeDir + Genome + ".scaffolds.fasta")));
+		out = new BufferedWriter(new FileWriter(new File(
+				OutDir + Group + "_" + Genome + "_scaffoldSizes.txt")));
+		out.write("scaffold\tlength\n");
+		line = fasta.readLine();
+		int len = 0;//length of the sequence
+		String scaff = "";
+		while(line != null) {
+			if(line.startsWith(">")) {
+				if(len != 0) {
+					out.write(scaff + "\t" + len + "\n");
+				}
+				scaff = line.replace(">", "");
+				len = 0;
+			} else {
+				len += line.length();
+			}
+			line = fasta.readLine();
+		}
+		fasta.close();
+		out.close();
+	}
+
+}
