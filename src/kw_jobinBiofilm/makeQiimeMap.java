90c0434a3e9f2518b4ae2ad248d0a24a75b1bbf1
kwinglee <kwinglee@uncc.edu>
2016-01-29 13:10:46 -0500

    generate qiime map, convert fastq to fasta

---
+++ src/kw_jobinBiofilm/makeQiimeMap.java
@@ -0,0 +1,27 @@
+/**
+ * generate mapping file for QIIME
+ */
+package kw_jobinBiofilm;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class makeQiimeMap {
+	public static String DIR = "/nobackup/afodor_research/kwinglee/jobin/biofilm/";
+	
+	public static void main(String[] args) throws IOException {
+		File[] fastas = new File(DIR + "join").listFiles();
+		BufferedWriter out = new BufferedWriter(new FileWriter(new File(DIR + "qiimeMap.txt")));
+		out.write("#SampleID\tBarcodeSequence\tLinkerPrimerSequence\tInputFileName\tDescription\n");
+		for(File f : fastas) {
+			String name = f.getName();
+			if(name.endsWith("join.fasta")) {
+				String id = name.replaceAll("join.fasta", "").replaceAll("biofilm_", "");
+				out.write(id + "\tX\tX\t" + name + "\n");
+			}
+		}
+		out.close();
+	}
+}
