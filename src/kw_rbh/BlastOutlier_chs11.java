d609b280a209c4b59055bd5bcd315c34f4bc067d
kwinglee <kwinglee@uncc.edu>
2016-02-29 16:38:34 -0500

    shorten name

--- src/kw_rbh/BlastOutlier_chs11.java
+++ src/kw_rbh/BlastOutlier_chs11.java
@@ -1,5 +1,5 @@
 /*
- * For orthologGroup outliers identified from all v. all MDS plot, blast
+ * For chs11 outliers identified from all v. all MDS plot, blast
  */
 package kw_rbh;
 
@@ -32,15 +32,17 @@ public class BlastOutlier_chs11 {
 		BufferedWriter runAll = new BufferedWriter(new FileWriter(new File(
 				BLAST_DIR + "runAll_orthogroup.sh")));
 		for(String gene : outliers) {
+			String[] g = gene.split("_");
+			String name = g[g.length-2] + "_" + g[g.length-1];
 			BufferedWriter script = new BufferedWriter(new FileWriter(new File(
 					BLAST_DIR + "nrBLAST_" + gene)));
 			script.write("module load blast\n");
 			script.write("blastn -query " + DIR + "geneFastas/carolina_klebsiella_pneumoniae_chs_11.0/" + gene + ".fasta"
 					+ " -db " + DB + " -outfmt 7 -out " +
-					BLAST_DIR + "/nrBLAST_" + gene + ".txt\n");
+					BLAST_DIR + "/nrBLAST_" + name + ".txt\n");
 			script.close();
 
-			runAll.write("qsub -q \"viper_batch\" nrBLAST_" + gene + "\n");
+			runAll.write("qsub -q \"viper_batch\" nrBLAST_" + name + "\n");
 		}
 		runAll.close();
 
