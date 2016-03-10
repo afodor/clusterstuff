c845c3d1a733b5310360cde7c2e7913ce60f83a3
Anthony Fodor <anthony.fodor@gmail.com>
2016-03-10 15:39:49 -0500

    first attempt at random k-mers; remove unused imports...

--- src/creOrthologs/kmers/RandomKMers.java
+++ src/creOrthologs/kmers/RandomKMers.java
@@ -5,16 +5,20 @@ import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.StringTokenizer;
+
+import utils.Translate;
 
 public class RandomKMers
 {
 	public static void main(String[] args) throws Exception
 	{
-		if( args.length != 3)
+		if( args.length != 4)
 		{
-			System.out.println("Usage numKMers inKmerPath fileOut");
+			System.out.println("Usage numKMers inKmerPath distFileOut keyFileOut");
 			System.exit(1);
 		}
 		
@@ -26,6 +30,75 @@ public class RandomKMers
 		
 		System.out.println("Shuffled");
 		
+		HashSet<String> set = new HashSet<String>();
+		
+		int index=0;
+		
+		int numKmers = Integer.parseInt(args[0]);
+		
+		// will throw if there an insufficient number of kmers
+		while( set.size() < numKmers )
+		{
+			set.add(kmerList.get(index));
+			index++;
+		}
+		
+		HashMap<String, HashMap<String,Integer>> bigMap = getBigMap(set);
+		ConstrainKMersToRegion.writeDistanceMatrix(bigMap, new File( args[2]), new File(args[3]));
+	}
+	
+	// outer key is the genome name; inner key is the k-mer
+	private static HashMap<String, HashMap<String,Integer>> getBigMap(HashSet<String> constrainingSet) throws Exception
+	{
+		HashMap<String, HashMap<String,Integer>> map = new HashMap<String, HashMap<String,Integer>>();
+		System.out.println("Got constraining set with " + constrainingSet.size());
+			
+		for(String s : MakeKmers.KMER_DIR.list() )
+			if( s.endsWith("_kmers.txt"))
+			{
+				String shortName = s.replace("_kmers.txt", "");
+				HashMap<String, Integer> innerMap = new HashMap<String,Integer>();
+				map.put(shortName,innerMap);
+					
+				BufferedReader reader = new BufferedReader(new FileReader(
+					new File(MakeKmers.KMER_DIR.getAbsolutePath() + File.separator + s)));
+					
+				for(String s2= reader.readLine(); s2 != null; s2= reader.readLine())
+				{
+					String[] splits = s2.split("\t");
+						
+					if( splits.length !=2 )
+						throw new Exception("No");
+						
+					String key = null;
+						
+					if( constrainingSet.contains(splits[0]) )
+					{
+						key = splits[0];
+					}
+					else
+					{
+						String reverse = Translate.reverseTranscribe(splits[0]);
+							
+						if( constrainingSet.contains(reverse))
+						{
+								key = reverse;
+						}
+					}
+						
+					if( key != null)
+					{
+						if( innerMap.containsKey(key))
+							throw new Exception("No");
+							
+						innerMap.put(key, Integer.parseInt(splits[1]));
+					}
+				}
+					
+				reader.close();
+			}
+			
+			return map;
 	}
 	
 	private static List<String> getKmerList(String inKmerFilePath) throws Exception
--- src/kw_creOrthologs/SpearmansAcrossAll_orthologGroups.java
+++ src/kw_creOrthologs/SpearmansAcrossAll_orthologGroups.java
@@ -11,7 +11,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.StringTokenizer;
-import creOrthologs.kmers.*;
 
 import utils.Spearman;
 
