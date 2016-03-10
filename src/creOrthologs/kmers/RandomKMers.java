32b5dde15e5ccdcfdca2e88c1f61eb89541d481f
Anthony Fodor <anthony.fodor@gmail.com>
2016-03-10 13:26:23 -0500

    remove a stray import...

--- src/creOrthologs/kmers/RandomKMers.java
+++ src/creOrthologs/kmers/RandomKMers.java
@@ -5,7 +5,6 @@ import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.StringTokenizer;
 
