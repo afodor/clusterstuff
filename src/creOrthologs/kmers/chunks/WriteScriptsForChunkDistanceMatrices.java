a572b137dba67866ceb5dc9c05ac8760aac3790b
afodor <anthony.fodor@gmail.com>
2016-03-17 19:11:21 -0400

    clean up the code...

--- src/creOrthologs/kmers/chunks/WriteScriptsForChunkDistanceMatrices.java
+++ src/creOrthologs/kmers/chunks/WriteScriptsForChunkDistanceMatrices.java
@@ -11,12 +11,35 @@ public class WriteScriptsForChunkDistanceMatrices
 	private static final File CHUNK_KMER_SCRIPT_DIR = 
 			new File( "/nobackup/afodor_research/af_broad/scripts/runKmerChunks");
 	
+	public static final String GENOME_PATH= 
+			"/nobackup/afodor_research/af_broad/carolina/klebsiella_pneumoniae_chs_11.0.scaffolds.fasta";
+	
+	public static final String CONTIG = "7000000220927538";
+	
+	
+	private static void writeOne(BufferedWriter allWriter, int startPos, int endPos, int index) 
+		throws Exception
+	{
+		File shFile = new File(
+				CHUNK_KMER_SCRIPT_DIR.getAbsolutePath() + File.separator + "run_" + index + ".txt"	);
+		
+		BufferedWriter aWriter = new BufferedWriter(new FileWriter(shFile));
+		
+		allWriter.write("qsub -q \"viper\" " +  shFile.getAbsolutePath());
+		
+		aWriter.write("java -cp /users/afodor/gitInstall/clusterstuff/bin "
+				+ "creOrthologs.kmers.ConstrainKMersToRegion "  + 
+				GENOME_PATH+ " " + CONTIG+ " " +  startPos+ " " 
+				+ endPos+ "\n");
+		
+		allWriter.flush(); 
+		
+		aWriter.flush();  aWriter.close();
+	
+	}
+	
 	public static void main(String[] args) throws Exception
 	{
-		String genomePath = 
-				"/nobackup/afodor_research/af_broad/carolina/klebsiella_pneumoniae_chs_11.0.scaffolds.fasta";
-		
-		String contig = "7000000220927538";
 		
 		BufferedWriter allWriter  = new BufferedWriter(new FileWriter(new File(
 			CHUNK_KMER_SCRIPT_DIR.getAbsolutePath() + File.separator + 
@@ -32,47 +55,21 @@ public class WriteScriptsForChunkDistanceMatrices
 		for(String s = reader.readLine() ; s != null; s = reader.readLine())
 		{
 			String[] splits = s.split("\t");
+
+			if( splits.length != 4)
+				throw new Exception("No");
 			
 			if( lastSplits != null )
 			{
-				File shFile = new File(
-						CHUNK_KMER_SCRIPT_DIR.getAbsolutePath() + File.separator + "run_" + index + ".txt"	);
-				
-				BufferedWriter aWriter = new BufferedWriter(new FileWriter(shFile));
-				
-				allWriter.write("qsub -q \"viper\" " +  shFile.getAbsolutePath());
-				
-				aWriter.write("java -cp /users/afodor/gitInstall/clusterstuff/bin "
-						+ "creOrthologs.kmers.ConstrainKMersToRegion "  + 
-						genomePath + " " + contig + " " +  (Integer.parseInt(lastSplits[1]) + 6000) + " " 
-						+ (Integer.parseInt(splits[0]) -1000) + "\n");
-				
-				allWriter.flush(); 
-				
-				aWriter.flush();  aWriter.close();
+				writeOne(allWriter, (Integer.parseInt(lastSplits[1]) + 6000), 
+					(Integer.parseInt(splits[0]) -1000) , index);
 				
 				index++;
 			
 			}
 			
-			if( splits.length != 4)
-				throw new Exception("No");
-			
-			File shFile = new File(
-					CHUNK_KMER_SCRIPT_DIR.getAbsolutePath() + File.separator + "run_" + index + ".txt"	);
-			
-			BufferedWriter aWriter = new BufferedWriter(new FileWriter(shFile));
-			
-			allWriter.write("qsub -q \"viper\" " +  shFile.getAbsolutePath());
-			
-			aWriter.write("java -cp /users/afodor/gitInstall/clusterstuff/bin "
-					+ "creOrthologs.kmers.ConstrainKMersToRegion "  + 
-					genomePath + " " + contig + " " +  Integer.parseInt(splits[0]) + " " 
-					+ (Integer.parseInt(splits[1]) + 5000 ) + "\n");
-			
-			allWriter.flush();  allWriter.close();
-			aWriter.flush();  aWriter.close();
-			
+			writeOne( allWriter,
+					Integer.parseInt(splits[0]), Integer.parseInt(splits[1]) + 5000, index);
 			index++;
 			lastSplits = splits;
 		}
