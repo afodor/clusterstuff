a38592f0407457170ffc63bb3ee2d332267aa61a
kwinglee <kwinglee@uncc.edu>
2016-05-12 16:04:37 -0400

    compare human filtered and unfiltered metaphlan runs

---
+++ src/kw_china_wgs_metaphlan/CompareMetaphlanRuns.java
@@ -0,0 +1,30 @@
+/*
+ * compare metaphlan runs with and without filtering human reads
+ */
+package kw_china_wgs_metaphlan;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class CompareMetaphlanRuns {
+	public static String DIR = "/nobackup/afodor_research/kwinglee/china/wgs/";
+	
+	public static void main(String[] args) throws IOException {
+		String filterDir = DIR + "metaphlanResultsFilterHuman/";
+		String otherDir = DIR + "metaphlanResults/";
+		String[] files = new File(filterDir).list();
+		BufferedWriter script = new BufferedWriter(new FileWriter(new File(
+				DIR + "metScripts/compare.sh")));
+		for(String f : files) {
+			if(f.startsWith("metaphlan_table_filterHuman")) {
+				String sample = f.replace("metaphlan_table_filterHuman_", "");
+				script.write("diff " + filterDir + "metaphlan_table_filterHuman_"
+						+ sample + " " + otherDir + "metaphlan_table_" + sample + "\n");
+			}
+		}
+		script.close();
+	}
+
+}
