b23fafe12d3acb8f4ba81c7cf5f2e99f399b4d59
Anthony Fodor <anthony.fodor@gmail.com>
2016-06-05 18:16:58 -0400

    scripts for test code...

---
+++ src/cmcDistances/test/WriteContextScripts.java
@@ -0,0 +1,55 @@
+package cmcDistances.test;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+
+public class WriteContextScripts
+{
+	private static File INPUT_DIRECTORY = new File(
+			"/projects/afodor_chs/fromNury5_2016");
+	
+	static File OUTPUT_DIRECTORY = new File(
+			"/nobackup/afodor_research/fromNury52016/test/context");
+	
+	private static File SCRIPT_DIRECTORY = new File(
+			"/nobackup/afodor_research/fromNury52016/test/scripts");
+	
+	public static void main(String[] args) throws Exception
+	{
+		BufferedWriter allWriter = new BufferedWriter(new FileWriter(new File(
+			SCRIPT_DIRECTORY + File.separator + "runAll.sh"	)));
+		
+		String[] files = INPUT_DIRECTORY.list();
+		
+		int index = 1;
+		
+		for(String s : files)
+		{
+			if ( s.endsWith(".fastq"))
+			{
+				File inFile = new File(INPUT_DIRECTORY.getAbsolutePath() + File.separator + s);
+				
+				File outFile = new File(OUTPUT_DIRECTORY.getAbsolutePath() + File.separator + 
+						s.replaceAll(".fastq", "") + ".context");
+						
+				File scriptFile = new File(SCRIPT_DIRECTORY.getAbsolutePath() + File.separator 
+						+ "run_" + index + ".sh");
+				
+				BufferedWriter writer = new BufferedWriter(new FileWriter(scriptFile));
+				
+				writer.write("java  -Xmx20g -cp /users/afodor/gitInstall/clusterstuff/bin cmcDistances.test.VerifyContif " + 
+								 inFile.getAbsolutePath() + " " + outFile.getAbsolutePath() + " 15 15" 
+								+"\n"
+						);
+				
+				allWriter.write("qsub -q\"viper\" " + scriptFile.getAbsolutePath() + "\n");
+				
+				writer.flush();  writer.close();
+				index++;
+			}
+		}
+		
+		allWriter.flush();  allWriter.close();
+	}
+}
