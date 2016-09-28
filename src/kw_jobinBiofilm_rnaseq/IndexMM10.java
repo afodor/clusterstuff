9d57a147d7c2b10e4b5ea205d611c30493a5a650
kwinglee <kwinglee@uncc.edu>
2016-09-28 14:56:17 -0400

    order files

--- src/kw_jobinBiofilm_rnaseq/IndexMM10.java
+++ src/kw_jobinBiofilm_rnaseq/IndexMM10.java
@@ -8,6 +8,8 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class IndexMM10 {
 	public static void main(String[] args) throws IOException {
@@ -15,8 +17,9 @@ public class IndexMM10 {
 		BufferedWriter script = new BufferedWriter(new FileWriter(new File(
 				"/projects/afodor_research/kwinglee/scripts/jobin/biofilmRNAseq/indexMM10")));
 		script.write("module load bwa\n");
-		script.write("cd " + mmdir);
+		script.write("cd " + mmdir + "\n");
 		String[] files = new File(mmdir).list();
+		Arrays.sort(files);
 		script.write("cat");
 		for(String f : files) {
 			if(f.endsWith(".fa") && !f.contains("_") && !f.contains("random")) {
