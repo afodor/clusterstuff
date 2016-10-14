e8ce33500487824a49392fa2435b47f4247a9c14
kwinglee <kwinglee@uncc.edu>
2016-10-14 11:16:15 -0400

    sort and fix id

--- src/kw_jobinApcTumor/MakeQiimeMap.java
+++ src/kw_jobinApcTumor/MakeQiimeMap.java
@@ -7,12 +7,14 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class MakeQiimeMap {
 	public static String DIR = "/nobackup/afodor_research/kwinglee/jobin/apcTumor/";
 	
 	public static void main(String[] args) throws IOException {
 		String[] files = new File(DIR + "joinedReads").list();
+		Arrays.sort(files);
 		BufferedWriter out = new BufferedWriter(new FileWriter(new File(DIR + "qiime/qiimeMap.txt")));
 		out.write("#SampleID\tBarcodeSequence\tLinkerPrimerSequence\tInputFileName\tDescription\n");
 		for(String name : files) {
@@ -20,7 +22,7 @@ public class MakeQiimeMap {
 					!name.contains("water") && 
 					!name.contains("PCR") &&
 					!name.contains("-")) { //only want the mouse samples; no controls, pancreas or biopsy
-				String id = name.replace(".join.fasta", "");
+				String id = name.replace("join.fasta", "");
 				out.write(id + "\tX\tX\t" + name + "\n");
 			}
 		}
