638f5b02ca12fbb76a2a3ce09e0c9761bc665f36
kwinglee <kwinglee@uncc.edu>
2016-12-08 13:17:43 -0500

    scripts to download T2D from ENA

---
+++ src/kw_machineLearning/DownloadT2Dena.java
@@ -0,0 +1,66 @@
+/*
+ * Download the cirrhosis dataset from ENA
+ */
+package kw_machineLearning;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class DownloadT2Dena {
+	public static String DIR = "/nobackup/afodor_research/kwinglee/machineLearning/t2d/";
+	public static int NUMCMDS = 10;//number of downloads per script
+	public static String SCRIPTDIR = "/projects/afodor_research/kwinglee/scripts/machineLearning/t2d/";
+
+	public static void main(String[] args) throws IOException {
+		//set up scripts
+		BufferedWriter runAll = new BufferedWriter(new FileWriter(new File(
+				SCRIPTDIR + "downloadAllENA.sh")));
+		String scriptBase = "dwnldenaT2D_";
+		BufferedWriter script = new BufferedWriter(new FileWriter(new File(
+				SCRIPTDIR + scriptBase + "0")));
+		int numHours = 5 * NUMCMDS;
+		script.write("#PBS -l walltime=" + Integer.toString(numHours) + ":00:00\n");
+		script.write("cd " + DIR + "fastqs/\n");
+		runAll.write("qsub -q \"copperhead\" " + scriptBase + "0\n");
+		int numScripts = 1;
+		int numCmds = 0;
+
+		String[] metaList = new String[]{"SRP008047.txt", "SRP011011.txt"};
+		for(String meta : metaList) {
+			BufferedReader list = new BufferedReader(new FileReader(new File(
+					DIR + meta)));//contains the list of read files
+			list.readLine();//header
+			for(String line = list.readLine(); line!= null; line = list.readLine()) {
+				String[] sp = line.split("\t");
+
+				String fqs = sp[9];
+				String[] files = fqs.split(";");
+
+				//add to script
+				script.write("wget " + files[0] + "\n");
+				script.write("wget " + files[1] + "\n");
+				numCmds+=2;
+
+				//check if need new script
+				if(numCmds == NUMCMDS) {
+					script.close();
+					script = new BufferedWriter(new FileWriter(new File(
+							SCRIPTDIR + scriptBase + numScripts)));
+					script.write("#PBS -l walltime=" + Integer.toString(numHours) + ":00:00\n");
+					script.write("cd " + DIR + "fastqs/\n");
+					runAll.write("qsub -q \"copperhead\" " + scriptBase + numScripts + "\n");
+					numScripts++;
+					numCmds = 0;
+				}
+			}
+			list.close();
+		}
+		runAll.close();
+		script.close();
+		System.out.println("Number of scripts: " + numScripts);
+	}
+}
