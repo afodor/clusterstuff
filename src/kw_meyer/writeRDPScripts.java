312cba21b532d885776d6ad3eddc26c8624beb2d
kwinglee <kwinglee@uncc.edu>
2016-12-09 14:21:46 -0500

    Run trimmed CARDIA data through RDP

--- src/kw_meyer/writeRDPScripts.java
+++ src/kw_meyer/writeRDPScripts.java
@@ -10,8 +10,12 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class writeRDPScripts {
-	public static final String FASTA_FOLDER = "/nobackup/afodor_research/katieDataSep21/inFasta/";
-	public static final String RDP_FOLDER = "/nobackup/afodor_research/kwinglee/meyer/rdpResults/";
+	//directories for merged reads:
+	//public static final String FASTA_FOLDER = "/nobackup/afodor_research/katieDataSep21/inFasta/";
+	//public static final String RDP_FOLDER = "/nobackup/afodor_research/kwinglee/meyer/rdpResults/";
+	//directories for merged reads with primers removed:
+	public static final String FASTA_FOLDER = "/nobackup/afodor_research/kwinglee/meyer/fastas_merged_trimmed";
+	public static final String RDP_FOLDER = "/nobackup/afodor_research/kwinglee/meyer/rdpResults_trimmed/";
 	public static final String SCRIPT_FOLDER = "/projects/afodor_research/kwinglee/scripts/meyer/";
 
 	public static void main(String[] args) throws IOException {
@@ -20,8 +24,9 @@ public class writeRDPScripts {
 		File[] files = ffolder.listFiles();
 
 		//writes script to launch all other scripts
-		BufferedWriter allWriter = new BufferedWriter(new FileWriter(new File(SCRIPT_FOLDER + "runAll.sh")));
-
+		//BufferedWriter allWriter = new BufferedWriter(new FileWriter(new File(SCRIPT_FOLDER + "runAll.sh")));
+		BufferedWriter allWriter = new BufferedWriter(new FileWriter(new File(SCRIPT_FOLDER + "runAllTrim.sh")));
+		
 		for(int i = 0; i < files.length; i++) {
 			String name = files[i].getName().replace(".fna", "");
 			//write script to run RDP on that file
