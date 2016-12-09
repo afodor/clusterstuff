1ecd7861b002460b70882ecbcfa533e8811c89b1
kwinglee <kwinglee@uncc.edu>
2016-12-09 10:38:54 -0500

    write script to re-download failed HMP stool samples

--- src/kw_machineLearning/CheckHMPmd5.java
+++ src/kw_machineLearning/CheckHMPmd5.java
@@ -4,8 +4,10 @@
 package kw_machineLearning;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -13,10 +15,13 @@ import java.util.Set;
 
 public class CheckHMPmd5 {
 	public static String DIR = "/nobackup/afodor_research/kwinglee/machineLearning/hmp/";
-
+	public static String SCRIPTDIR = "/projects/afodor_research/kwinglee/scripts/machineLearning/hmp/";
+	public static String WEBSITE = "http://hmpdacc.org";
+	
 	public static void main(String[] args) throws IOException {
 		//get map of ID to HMP MD5
-		HashMap<String, String> hmp = new HashMap<String, String>();
+		HashMap<String, String> hmp = new HashMap<String, String>();//map of id to MD5
+		HashMap<String, String> link = new HashMap<String, String>();//map of id to download link
 		BufferedReader list = new BufferedReader(new FileReader(new File
 				(DIR + "HMIWGS_healthy.csv")));
 		for(String line = list.readLine(); line != null; line = list.readLine()) {
@@ -24,6 +29,7 @@ public class CheckHMPmd5 {
 			String[] sp = line.split(",");
 			if(sp[1].equals("stool")) {
 				hmp.put(sp[0], sp[3]);
+				link.put(sp[0], sp[2]);
 			}
 		}
 		list.close();
@@ -62,10 +68,25 @@ public class CheckHMPmd5 {
 		
 		//check MD5s
 		Set<String> keys = hmp.keySet();
+		Set<String> fail = new HashSet<String>();
 		for(String k : keys) {
 			if(!hmp.get(k).equals(download.get(k))) {
 				System.err.println("Bad download: " + k);
+				fail.add(k);
 			}
+		}
+		System.out.println("Number failed: " + fail.size());
+		
+		//write script of failed downloads
+		if(!fail.isEmpty()) {
+			BufferedWriter script = new BufferedWriter(new FileWriter(new File(
+					SCRIPTDIR + "redownload")));
+			script.write("#PBS -l walltime=" + Integer.toString(fail.size() * 5) + ":00:00\n");
+			script.write("cd " + DIR + "fastqs/stool_download2/\n");
+			for(String k : fail) {
+				script.write("wget " + WEBSITE + link.get(k) + "\n");
+			}
+			script.close();
 		}
 	}
 }
