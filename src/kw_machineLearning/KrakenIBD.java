969adc01364b67ff4bd8a8b7412a96f09cbca1ea
kwinglee <kwinglee@uncc.edu>
2016-12-12 13:37:08 -0500

    run minikraken on IBD

--- src/kw_machineLearning/KrakenIBD.java
+++ src/kw_machineLearning/KrakenIBD.java
@@ -9,24 +9,33 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class StandardKrakenIBD {
+public class KrakenIBD {
 	public static String KRAKEN_DIR = "/nobackup/afodor_research/kwinglee/software/kraken/";
-	public static String DB = KRAKEN_DIR + "krakenStandardDB";
+	//public static String DB = KRAKEN_DIR + "krakenStandardDB";
+	public static String DB = KRAKEN_DIR + "minikraken_20141208";
 	public static String FASTQ_DIR = "/nobackup/afodor_research/kwinglee/machineLearning/ibd/fastqs/";
 	public static String SCRIPT_DIR = "/projects/afodor_research/kwinglee/scripts/machineLearning/ibd/";
-	public static String OUT_DIR = "/nobackup/afodor_research/kwinglee/machineLearning/ibd/stdKraken/";
+	//public static String OUT_DIR = "/nobackup/afodor_research/kwinglee/machineLearning/ibd/stdKraken/";
+	public static String OUT_DIR = "/nobackup/afodor_research/kwinglee/machineLearning/ibd/minikraken/";
 	
 	public static void main(String[] args) throws IOException {
+		File out = new File(OUT_DIR);
+		if(!out.exists()) {
+			out.mkdirs();
+		}
 		//set up script to run everything
 		BufferedWriter script = new BufferedWriter(new FileWriter(new File(
-				SCRIPT_DIR + "stdKrakenIBD")));
+				//SCRIPT_DIR + "stdKrakenIBD")));
+				SCRIPT_DIR + "minikrakenIBD")));
 		script.write("#PBS -l walltime=600:00:00\n");
-		script.write("#PBS -l mem=500GB\n");
+		//script.write("#PBS -l mem=500GB\n");
+		script.write("#PBS -l mem=10GB\n");
 		String[] fastqs = new File(FASTQ_DIR).list();
 		for(String fq : fastqs) {
 			if(fq.endsWith(".1.fq.gz")) {
 				String name = fq.replace(".1.fq.gz", "");
-				String seqName = OUT_DIR + name + "_stdKraken";
+				//String seqName = OUT_DIR + name + "_stdKraken";
+				String seqName = OUT_DIR + name + "_minikraken";
 				
 				//run kraken
 				script.write(KRAKEN_DIR + "kraken --preload --db " 
