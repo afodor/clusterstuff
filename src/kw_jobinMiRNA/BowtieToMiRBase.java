e03066bbe9f54e216f34912c0c055a6996db4b67
kwinglee <kwinglee@uncc.edu>
2016-12-13 11:42:45 -0500

    add hairpin and flagstat

--- src/kw_jobinMiRNA/BowtieToMiRBase.java
+++ src/kw_jobinMiRNA/BowtieToMiRBase.java
@@ -11,16 +11,21 @@ import java.io.IOException;
 public class BowtieToMiRBase {
 	public static String DIR = "/nobackup/afodor_research/kwinglee/jobin/microRNA/";
 	public static String FQDIR = DIR + "fastqs/";
-	public static String OUTDIR = DIR + "miRBaseAlign/";
+	public static String OUTDIR = DIR + "miRBaseBowtie/";
 	public static String SCRIPTDIR = "/projects/afodor_research/kwinglee/scripts/jobin/microRNA/";
-	public static String REF = "/nobackup/afodor_research/kwinglee/mirbase_v21/matureBowtie";
+	public static String MATREF = "/nobackup/afodor_research/kwinglee/mirbase_v21/matureBowtie";
+	public static String PINREF = "/nobackup/afodor_research/kwinglee/mirbase_v21/hairpinBowtie";
 	
 	public static void main(String[] args) throws IOException {
+		File odir = new File(OUTDIR);
+		if(!odir.exists()) {
+			odir.mkdirs();
+		}
 		File[] fqs = new File(FQDIR).listFiles();
 		BufferedWriter all = new BufferedWriter(new FileWriter(new File(
-				SCRIPTDIR + "miRalignAll.sh")));
+				SCRIPTDIR + "bowtieAlignAll.sh")));
 		for(File fq : fqs) {
-			String name = "miRalign_" + fq.getName().split("-")[0];
+			String name = "bowtieAlign_" + fq.getName().split("-")[0];
 			
 			//add to run all script
 			all.write("qsub -q \"copperhead\" " + name + "\n");
@@ -28,9 +33,19 @@ public class BowtieToMiRBase {
 			//write script
 			BufferedWriter script = new BufferedWriter(new FileWriter(new File(
 					SCRIPTDIR + name)));
+			script.write("#PBS -l procs=1");
 			script.write("module load bowtie2\n");
-			script.write("bowtie2 -x " + REF + " -U " + fq.getAbsolutePath()
-					+ " -S " + OUTDIR + name + "\n");
+			script.write("module load samtools\n");
+			//align to mature
+			script.write("bowtie2 -x " + MATREF + " -U " + fq.getAbsolutePath()
+					+ " -S " + OUTDIR + name + ".mature.sam\n");
+			script.write("samtools flagstat " + OUTDIR + name + ".mature.sam > " 
+					+ OUTDIR + name + ".mature.flagstat\n");
+			//align to hairpin
+			script.write("bowtie2 -x " + PINREF + " -U " + fq.getAbsolutePath()
+					+ " -S " + OUTDIR + name + ".hairpin.sam\n");//align to hairpin
+			script.write("samtools flagstat " + OUTDIR + name + ".hairpin.sam > " 
+					+ OUTDIR + name + ".hairpin.flagstat\n");
 			script.close();
 		}
 		
