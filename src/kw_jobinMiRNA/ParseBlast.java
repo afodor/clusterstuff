a556ccfb1e97f1d6e0709a17280f75405d935b50
kwinglee <kwinglee@uncc.edu>
2016-12-13 17:05:12 -0500

    get combined counts

--- src/kw_jobinMiRNA/ParseBlast.java
+++ src/kw_jobinMiRNA/ParseBlast.java
@@ -22,22 +22,42 @@ public class ParseBlast {
 	public static void main(String[] args) throws IOException {
 		String[] dirs = new String[]{"miRBaseBlast/", "miRBaseBlastReadsAsDB/"};
 		int[] qids = new int[]{0,1};
+		HashMap<String, HashSet<String>> matComb = new HashMap<String, HashSet<String>>();//id -> set of reads mapped
+		HashMap<String, HashSet<String>> hairComb = new HashMap<String, HashSet<String>>();//id -> set of reads mapped
 		for(int i = 0; i < dirs.length; i++) {
 			String d = dirs[i];
 			HashMap<String, Integer> mature = new HashMap<String, Integer>();//id -> number of reads mapped
 			HashMap<String, Integer> hairpin = new HashMap<String, Integer>();//id -> number of reads mapped
 			File[] files = new File(DIR + d).listFiles();
 			for(File f : files) {
+				String id = f.getName().split("//.")[0];
 				if(f.getName().endsWith(".hairpin.txt")) {
-					getCounts(f, hairpin, qids[i]);
+					HashSet<String> reads = getCounts(f, hairpin, qids[i]);
+					if(hairComb.containsKey(id)) {
+						HashSet<String> prev = hairComb.get(id);
+						prev.addAll(reads);
+						hairComb.put(id, prev);
+					} else {
+						hairComb.put(id, reads);
+					}
 				} else if(f.getName().endsWith(".mature.txt")) {
-					getCounts(f, mature, qids[i]);
+					HashSet<String> reads = getCounts(f, mature, qids[i]);
+					if(matComb.containsKey(id)) {
+						HashSet<String> prev = matComb.get(id);
+						prev.addAll(reads);
+						matComb.put(id, prev);
+					} else {
+						matComb.put(id, reads);
+					}
 				}
 			}
 			
 			writeCounts(mature, new File(DIR + d + "BlastMature.txt"));
 			writeCounts(hairpin, new File(DIR + d + "BlastHairpin.txt"));
 		}
+		
+		writeCountsCombined(matComb, new File(DIR + "BlastCombinedMature.txt"));
+		writeCountsCombined(matComb, new File(DIR + "BlastCombinedHairpin.txt"));
 	}
 
 	/*
@@ -54,13 +74,24 @@ public class ParseBlast {
 		}
 		out.close();
 	}
+	
+	private static void writeCountsCombined(HashMap<String, HashSet<String>> map, File file) throws IOException {
+		ArrayList<String> samples = new ArrayList<String>(map.keySet());
+		Collections.sort(samples);
+		BufferedWriter out = new BufferedWriter(new FileWriter(file));
+		out.write("sampleID\tnumReadsMapped\n");
+		for(String s : samples) {
+			out.write(s + "\t" + map.get(s).size() + "\n");
+		}
+		out.close();
+	}
 
 	/*
 	 * file = file to read
 	 * map = map to add results to
 	 * qseqid = which column contains query sequence id
 	 */
-	private static void getCounts(File file, HashMap<String, Integer> map,
+	private static HashSet<String> getCounts(File file, HashMap<String, Integer> map,
 			int qseqid) throws IOException {
 		HashSet<String> reads = new HashSet<String>();
 		BufferedReader br = new BufferedReader(new FileReader(file));
@@ -74,5 +105,6 @@ public class ParseBlast {
 		}
 		br.close();		
 		map.put(file.getName().split("\\.")[0], reads.size());
+		return(reads);
 	}
 }
