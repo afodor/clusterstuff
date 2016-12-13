12ce7508ba13c0df6e34f9832fc063895d0c1adc
kwinglee <kwinglee@uncc.edu>
2016-12-13 13:41:25 -0500

    split into multiple scripts

--- src/kw_machineLearning/UnzipHMP.java
+++ src/kw_machineLearning/UnzipHMP.java
@@ -11,19 +11,37 @@ import java.io.IOException;
 public class UnzipHMP {
 	public static String FASTQ_DIR = "/nobackup/afodor_research/kwinglee/machineLearning/hmp/fastqs/stool/";
 	public static String SCRIPT_DIR = "/projects/afodor_research/kwinglee/scripts/machineLearning/hmp/";
+	public static int MAXCMDS = 5;
 	
 	public static void main(String[] args) throws IOException {
-		BufferedWriter out = new BufferedWriter(new FileWriter(new File(
-				SCRIPT_DIR + "unzipHMP")));
-		out.write("#PBS -l procs=1\n");
-		out.write("cd " + FASTQ_DIR + "\n");
+		int numCmds = 0;
+		int numScripts = 0;
+		String sbase = "unzipHMP_";
+		BufferedWriter all = new BufferedWriter(new FileWriter(new File(
+				SCRIPT_DIR + "unzipAll.sh")));
+		all.write("qsub -q \"copperhead\" " + sbase + numScripts);
+		BufferedWriter script = new BufferedWriter(new FileWriter(new File(
+				SCRIPT_DIR + sbase + numScripts)));
+		script.write("#PBS -l procs=1\n");
+		script.write("cd " + FASTQ_DIR + "\n");
 		String[] files = new File(FASTQ_DIR).list();
 		for(String f : files) {
 			if(f.endsWith(".tar.bz2")) {
-				out.write("tar -vxjf " + f + "\n");
+				script.write("tar -vxjf " + f + "\n");
+				numCmds++;
+				if(numCmds == MAXCMDS) {
+					numScripts++;
+					script.close();
+					script = new BufferedWriter(new FileWriter(new File(
+							SCRIPT_DIR + sbase + numScripts)));
+					script.write("#PBS -l procs=1\n");
+					script.write("cd " + FASTQ_DIR + "\n");
+					all.write("qsub -q \"copperhead\" " + sbase + numScripts);
+				}
 			}
 		}
-		out.close();
+		script.close();
+		all.close();
 	}
 
 }
