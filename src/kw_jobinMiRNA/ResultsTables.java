be82a1b3a6fc0efa6f5d3f5a3d95df816b799cee
kwinglee <kwinglee@uncc.edu>
2016-12-15 12:47:28 -0500

    don't analyze piR yet

--- src/kw_jobinMiRNA/ResultsTables.java
+++ src/kw_jobinMiRNA/ResultsTables.java
@@ -31,6 +31,51 @@ public class ResultsTables {
 		//analyzeJava();
 	}
 
+	private static void analyzeBowtie() throws IOException {
+		HashMap<String, Integer> miRmature = new HashMap<String, Integer>();
+		HashMap<String, Integer> miRhairpin = new HashMap<String, Integer>();
+		HashMap<String, Integer> mouse = new HashMap<String, Integer>();
+		HashMap<String, Integer> piR = new HashMap<String, Integer>();
+	
+		//miR
+		File[] files = new File(DIR + "miRBaseBowtie").listFiles();
+		for(File f : files) {
+			if(f.getName().endsWith(".sam")) {
+				String id = f.getName().split("\\.")[0];
+				int count = getBowtieCounts(f);
+				if(f.getName().contains("mature")) {
+					miRmature.put(id, count);
+				} else if(f.getName().contains("hairpin")) {
+					miRhairpin.put(id, count);
+				} else {
+					System.out.println("Extra sam: " + f.getName() + ' ' + count);
+				}
+			}
+		}
+	
+		//mouse
+		files = new File(DIR + "mouseBowtie/").listFiles();
+		for(File f : files) {
+			if(f.getName().endsWith(".sam")) {
+				mouse.put(f.getName().split("\\.")[0], getBowtieCounts(f));
+			}
+		}
+	
+		//piR
+		/*files = new File(DIR + "piRBaseBowtie/").listFiles();
+		for(File f : files) {
+			if(f.getName().endsWith(".sam")) {
+				piR.put(f.getName().split("\\.")[0], getBowtieCounts(f));
+			}
+		}*/
+		for(int i = 1; i <= 10; i++) {
+			String key = "Sample" + i;
+			piR.put(key, -1);
+		}
+	
+		writeTable("bowtie", miRmature, miRhairpin, mouse, piR);
+	}
+
 	private static void analyzeBlast() throws IOException {
 		HashMap<String, Integer> miRmature = new HashMap<String, Integer>();
 		HashMap<String, Integer> miRhairpin = new HashMap<String, Integer>();
@@ -65,26 +110,11 @@ public class ResultsTables {
 		files = new File(DIR + "piRBaseBlast").listFiles();
 		for(File f : files) {
 			if(f.getName().endsWith(".piR.blast.txt")) {
-				mouse.put(f.getName().split("\\.")[0], getBlastCounts(f));
+				piR.put(f.getName().split("\\.")[0], getBlastCounts(f));
 			}
 		}
 
 		writeTable("blast", miRmature, miRhairpin, mouse, piR);
-	}
-
-	private static Integer getBlastCounts(File file) throws IOException {
-		HashSet<String> reads = new HashSet<String>();
-		BufferedReader br = new BufferedReader(new FileReader(file));
-		for(String line = br.readLine(); line != null; line = br.readLine()) {
-			String[] sp = line.split("\t");
-			//qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore
-			if(Double.parseDouble(sp[2]) > PID &&
-					Integer.parseInt(sp[4]) <= MISMATCH) {
-				reads.add(sp[0]);
-			}
-		}
-		br.close();		
-		return(reads.size());
 	}
 
 	private static void analyzeJava() throws IOException {
@@ -92,78 +122,14 @@ public class ResultsTables {
 		HashMap<String, Integer> miRhairpin = new HashMap<String, Integer>();
 		HashMap<String, Integer> mouse = new HashMap<String, Integer>();
 		HashMap<String, Integer> piR = new HashMap<String, Integer>();
-
+	
 		//mouse
 		/*for(int i = 1; i <= 10; i++) {
 			String key = "Sample" + i;
 			mouse.put(key, NA);
 		}*/
-
+	
 		writeTable("java", miRmature, miRhairpin, mouse, piR);
-	}
-
-	private static void analyzeBowtie() throws IOException {
-		HashMap<String, Integer> miRmature = new HashMap<String, Integer>();
-		HashMap<String, Integer> miRhairpin = new HashMap<String, Integer>();
-		HashMap<String, Integer> mouse = new HashMap<String, Integer>();
-		HashMap<String, Integer> piR = new HashMap<String, Integer>();
-
-		//miR
-		File[] files = new File(DIR + "miRBaseBowtie").listFiles();
-		for(File f : files) {
-			if(f.getName().endsWith(".sam")) {
-				String id = f.getName().split("\\.")[0];
-				int count = getBowtieCounts(f);
-				if(f.getName().contains("mature")) {
-					miRmature.put(id, count);
-				} else if(f.getName().contains("hairpin")) {
-					miRhairpin.put(id, count);
-				} else {
-					System.out.println("Extra sam: " + f.getName() + ' ' + count);
-				}
-			}
-		}
-
-		//mouse
-		files = new File(DIR + "mouseBowtie/").listFiles();
-		for(File f : files) {
-			if(f.getName().endsWith(".sam")) {
-				mouse.put(f.getName().split("\\.")[0], getBowtieCounts(f));
-			}
-		}
-
-		//piR
-		files = new File(DIR + "piRBaseBowtie/").listFiles();
-		for(File f : files) {
-			if(f.getName().endsWith(".sam")) {
-				mouse.put(f.getName().split("\\.")[0], getBowtieCounts(f));
-			}
-		}
-
-		writeTable("bowtie", miRmature, miRhairpin, mouse, piR);
-	}
-
-	private static void writeTable(String name,
-			HashMap<String, Integer> miRmature,
-			HashMap<String, Integer> miRhairpin,
-			HashMap<String, Integer> mouse, HashMap<String, Integer> piR) throws IOException {
-		BufferedWriter out = new BufferedWriter(new FileWriter(new File(
-				DIR + "Results_" + name + ".txt")));
-		out.write("sampleID\tgroup\ttotalNumReads\tnumMapMiRmature\t"
-				+ "numMapMiRhairpin\tnumMapMouse\tnumMapPiR\n");
-		for(int i = 1; i <= 10; i++) {
-			String key = "Sample" + i;
-			out.write(key + "\t");
-			if(i <= 5) {
-				out.write("GF\t");
-			} else {
-				out.write("SPF\t");
-			}
-			out.write(NUMREADS.get(key) + "\t" + miRmature.get(key) + "\t" +
-					miRhairpin.get(key) + "\t" + mouse.get(key) + "\t" +
-					piR.get(key) + "\n");
-		}
-		out.close();		
 	}
 
 	private static int getBowtieCounts(File file) throws IOException {
@@ -193,5 +159,43 @@ public class ResultsTables {
 			NUMREADS.put(id, numReads);
 		}
 		return(mapped.size());
+	}
+
+	private static Integer getBlastCounts(File file) throws IOException {
+		HashSet<String> reads = new HashSet<String>();
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		for(String line = br.readLine(); line != null; line = br.readLine()) {
+			String[] sp = line.split("\t");
+			//qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore
+			if(Double.parseDouble(sp[2]) > PID &&
+					Integer.parseInt(sp[4]) <= MISMATCH) {
+				reads.add(sp[0]);
+			}
+		}
+		br.close();		
+		return(reads.size());
+	}
+
+	private static void writeTable(String name,
+			HashMap<String, Integer> miRmature,
+			HashMap<String, Integer> miRhairpin,
+			HashMap<String, Integer> mouse, HashMap<String, Integer> piR) throws IOException {
+		BufferedWriter out = new BufferedWriter(new FileWriter(new File(
+				DIR + "Results_" + name + ".txt")));
+		out.write("sampleID\tgroup\ttotalNumReads\tnumMapMiRmature\t"
+				+ "numMapMiRhairpin\tnumMapMouse\tnumMapPiR\n");
+		for(int i = 1; i <= 10; i++) {
+			String key = "Sample" + i;
+			out.write(key + "\t");
+			if(i <= 5) {
+				out.write("GF\t");
+			} else {
+				out.write("SPF\t");
+			}
+			out.write(NUMREADS.get(key) + "\t" + miRmature.get(key) + "\t" +
+					miRhairpin.get(key) + "\t" + mouse.get(key) + "\t" +
+					piR.get(key) + "\n");
+		}
+		out.close();		
 	}
 }
