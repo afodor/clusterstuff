994cb044f4637954773b15f76e98bdf99b2c360f
kwinglee <kwinglee@uncc.edu>
2016-12-16 15:12:08 -0500

    write tables

--- src/kw_machineLearning/MergeKrakenOutputColorectal.java
+++ src/kw_machineLearning/MergeKrakenOutputColorectal.java
@@ -39,13 +39,13 @@ public class MergeKrakenOutputColorectal {
 				metaMap.put(sampleID[i], disease[i]);
 			}
 		}
-		System.out.println("metadata " + metaMap.size());
+		/*System.out.println("metadata " + metaMap.size());
 		ArrayList<String> keys = new ArrayList<String>(metaMap.keySet());
 		Collections.sort(keys);
 		for(String key : keys) {
 			System.out.println(key + "\t" + metaMap.get(key));
 		}
-		System.out.println();
+		System.out.println();*/
 
 		//get list of files to read
 		ArrayList<String> tables = new ArrayList<String> ();
@@ -79,7 +79,7 @@ public class MergeKrakenOutputColorectal {
 		}
 
 		//check sequence ids
-		System.out.println("sequences " + tables.size());
+		/*System.out.println("sequences " + tables.size());
 		HashSet<String> seqs = new HashSet<String>();
 		for(int i = 0; i < tables.size(); i++) {
 			String id = tables.get(i).split("_")[0];
@@ -94,10 +94,10 @@ public class MergeKrakenOutputColorectal {
 			if(!seqs.contains(key)) {
 				System.out.println(key);
 			}
-		}
+		}*/
 		
 		//add missing samples and make map of sample id to sequences
-		/*HashMap<String, ArrayList<String>> sequences = new HashMap<String, ArrayList<String>>();//map of id to the sequences associated 
+		HashMap<String, ArrayList<String>> sequences = new HashMap<String, ArrayList<String>>();//map of id to the sequences associated 
 		for(int i = 0; i < tables.size(); i++) {
 			String id = tables.get(i).split("_")[0];
 			if(!metaMap.containsKey(id)) {
@@ -222,7 +222,7 @@ public class MergeKrakenOutputColorectal {
 		for(String s : seqIDs) {
 			out.write(s + "\t" + metaMap.get(s) + "\n");
 		}
-		out.close();*/
+		out.close();
 	}
 
 	//function that adds the given counts to the appropriate key in the given map
