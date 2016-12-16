c98cd65c83d4498cd6d9adf61d113f94612cad22
kwinglee <kwinglee@uncc.edu>
2016-12-16 17:13:52 -0500

    fix module load

--- src/kw_jobinMiRNA/KEGGblastxScripts.java
+++ src/kw_jobinMiRNA/KEGGblastxScripts.java
@@ -33,7 +33,7 @@ public class KEGGblastxScripts {
 				BufferedWriter script = new BufferedWriter(new FileWriter(new File(
 						SCRIPT_DIR + scriptName)));
 				script.write("#PBS -l walltime=300:00:00,mem=10GB,procs=1\n");
-				script.write("module load blast\2.5.0+n");
+				script.write("module load blast/2.5.0+\n");
 				script.write("blastx -outfmt 6 -db " + DB + " -query " +
 						f.getAbsolutePath() + " -out " +
 						OUTDIR + name + ".kegg.blast.txt\n");//blast command
