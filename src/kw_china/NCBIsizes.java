26b33bfee652f53ba4db9a4641f116f8ce56e553
kwinglee <kwinglee@uncc.edu>
2017-02-15 15:53:31 -0500

    get gene stats for each genome

---
+++ src/kw_china/NCBIsizes.java
@@ -0,0 +1,49 @@
+/*
+ * get a table of genome to number of genes and total gene length
+ */
+package kw_china;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class NCBIsizes {
+	public static void main(String[] args) throws IOException {
+		BufferedWriter out = new BufferedWriter(new FileWriter(new File(
+				"/nobackup/afodor_research/kwinglee/china/ncbiSizes.txt")));
+		File ncbiDir = new File("/nobackup/afodor_research/ncbi");
+		
+		out.write("genome\tnumberGenes\ttotalGeneLength\n");
+		File[] flist1 = ncbiDir.listFiles();
+		for(File f1 : flist1) {
+			if(f1.isDirectory()) {
+				File[] flist2 = f1.listFiles();
+				for(File f2 : flist2) {
+					if(f2.getName().endsWith(".frn")) {
+						BufferedReader fa = new BufferedReader(new FileReader(f2));
+						int numGenes = 0;
+						int totGeneLength = 0;
+						for(String line = fa.readLine(); line != null; line = fa.readLine()) {
+							if(line.startsWith(">")) {
+								numGenes++;
+							} else {
+								totGeneLength += line.length();
+							}
+						}
+						fa.close();
+						out.write(f1.getName() + "\t" + numGenes + 
+								"\t" + totGeneLength + "\n");
+					} else {
+						System.out.println("Skipped file: " + f2.getAbsolutePath());
+					}
+				}
+			}
+		}
+		
+		out.close();
+	}
+
+}
