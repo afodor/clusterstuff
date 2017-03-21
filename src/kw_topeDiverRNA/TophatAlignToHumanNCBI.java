eabd6abcf6b953e68a8acf519164ef41df9749ca
kwinglee <kwinglee@uncc.edu>
2017-03-21 14:30:15 -0400

    align to NCBI version of HG38 and generate cufflinks scripts

---
+++ src/kw_topeDiverRNA/TophatAlignToHumanNCBI.java
@@ -0,0 +1,52 @@
+/*
+ * Use Tophat2 to align all reads to human genome (NCBI version), using paired end info
+ */
+
+package kw_topeDiverRNA;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class TophatAlignToHumanNCBI {
+	public static String FQDIR = "/nobackup/afodor_research/topeRnaSeqFeb2017/170118_UNC31-K00269_0051_AHGWY3BBXX/";
+	public static String SCRIPTDIR = "/projects/afodor_research/kwinglee/scripts/tope/diverRNA/";
+	public static String OUTDIR = "/nobackup/afodor_research/kwinglee/tope/diverRNAseq/tophatAlignToHumanNCBI/";
+	public static String TOPHAT = "/nobackup/afodor_research/kwinglee/software/tophat-2.1.1.Linux_x86_64/tophat2";
+	public static String REF = "/nobackup/afodor_research/kwinglee/hg38/GCF_000001405.36_GRCh38.p10_genomic";
+	public static int NUM_THREADS = 4;
+
+	public static void main(String[] args) throws IOException {
+		BufferedWriter runAll = new BufferedWriter(new FileWriter(new File(
+				SCRIPTDIR + "runTophatAlignToHumanNCBI.sh")));
+		String[] fqs = new File(FQDIR).list();
+		for(String fq : fqs) {
+			if(fq.endsWith("_R1_001.fastq.gz")) {
+				String name = fq.split("_")[0].replaceAll("-$", "");
+				String scriptName = "ncbiTophatHuman_" + name;
+				BufferedWriter script = new BufferedWriter(new FileWriter(new File(
+						SCRIPTDIR + scriptName)));
+				//copperhead setup
+				script.write("#PBS -l walltime=600:00:00\n");
+				/*script.write("#PBS -l mem=10GB\n");
+				script.write("#PBS -l procs=" + NUM_THREADS + "\n");*/
+
+				//load needed modules
+				script.write("module load bowtie2\n");
+
+				//align
+				script.write(TOPHAT + " -o " + OUTDIR + name + " -p " + NUM_THREADS +
+						" " + REF + " " + FQDIR + fq + 
+						" " + FQDIR + fq.replace("_R1_", "_R2_") + "\n");
+
+				//add to run all
+				runAll.write("qsub -q \"Cobra\" " + scriptName + "\n");
+
+				script.close();
+			}
+		}
+		runAll.close();
+	}
+
+}
