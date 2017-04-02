1d3999cef820bef080b86f5f142c66e9f8c456d4
kwinglee <kwinglee@uncc.edu>
2017-04-02 15:34:28 -0400

    cufflinks scripts

--- src/kw_topeDiverRNA/CufflinksScriptsNCBI.java
+++ src/kw_topeDiverRNA/CufflinksScriptsNCBI.java
@@ -8,20 +8,20 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class CufflinksScripts {
+public class CufflinksScriptsNCBI {
 	private static String BASEDIR = "/nobackup/afodor_research/kwinglee/tope/diverRNAseq/";
-	private static String TOPHATDIR = BASEDIR + "tophatAlignToHuman/";
+	private static String TOPHATDIR = BASEDIR + "tophatAlignToHumanNCBI/";
 	private static String CUFF = "/nobackup/afodor_research/kwinglee/software/cufflinks-2.2.1.Linux_x86_64/cufflinks";
-	private static String OUTDIR = BASEDIR + "cufflinksHumanResults/";
+	private static String OUTDIR = BASEDIR + "cufflinksHumanResultsNCBI/";
 	private static String SCRIPTDIR = "/projects/afodor_research/kwinglee/scripts/tope/diverRNA/cufflinks/";
 	private static String GFF = "/nobackup/afodor_research/kwinglee/hg38/GCF_000001405.36_GRCh38.p10_genomic.gff";
 
 	public static void main(String[] args) throws IOException {
 		String[] sams = new File(TOPHATDIR).list();
 		BufferedWriter runAll = new BufferedWriter(new FileWriter(new File(
-				SCRIPTDIR + "runCufflinksTophat.sh")));
+				SCRIPTDIR + "runCufflinksTophatNCBI.sh")));
 		for(String s : sams) {
-			String scriptName = "cufflinks_" + s; 
+			String scriptName = "ncbiCufflinks_" + s; 
 			BufferedWriter script = new BufferedWriter(new FileWriter(new File(
 					SCRIPTDIR + scriptName)));
 			script.write("#PBS -l walltime=500:00:00\n");
@@ -30,7 +30,6 @@ public class CufflinksScripts {
 					File.separator + "accepted_hits.bam\n");
 			script.close();
 
-			//runAll.write("qsub -q \"copperhead\" " + scriptName + "\n");
 			runAll.write("qsub -q \"Cobra\" " + scriptName + "\n");
 		}
 		runAll.close();
