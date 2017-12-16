e002b27de3bece629de57ae407206181f58a0c68
Anthony Fodor <anthony.fodor@gmail.com>
2017-12-16 00:11:53 -0500

    remove duplicate path information...

--- src/chinaDec2017/CollectFasta.java
+++ src/chinaDec2017/CollectFasta.java
@@ -20,13 +20,13 @@ public class CollectFasta
 		for(File f : dirs)
 		{
 			File forwardFile = new File(f.getAbsolutePath() + File.separator + 
-					"DMP0" + f.getName().substring(1) + "_L1_" + f + "_1.fq.gz");
+					"DMP0" + f.getName().substring(1) + "_L1_" + f.getName() + "_1.fq.gz");
 			
 			if( ! forwardFile.exists())
 				throw new Exception("Could not find " + forwardFile);
 			
 			File backwardsFile = new File( f.getAbsolutePath() + File.separator + 
-					"DMP0" + f.getName().substring(1) + "_L1_" + f + "_2.fq.gz" );
+					"DMP0" + f.getName().substring(1) + "_L1_" + f.getName() + "_2.fq.gz" );
 			
 			if( ! backwardsFile.exists())
 				throw new Exception("Could not find "+ backwardsFile);
