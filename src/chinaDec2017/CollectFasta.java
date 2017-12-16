f80472d83a7759faa9ff50c2fa4dd17dbfc6da41
Anthony Fodor <anthony.fodor@gmail.com>
2017-12-16 03:44:33 -0500

    copy fastqs...

--- src/chinaDec2017/CollectFasta.java
+++ src/chinaDec2017/CollectFasta.java
@@ -6,6 +6,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.InputStreamReader;
+import java.nio.file.CopyOption;
+import java.nio.file.Files;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.zip.GZIPInputStream;
@@ -15,6 +17,7 @@ import parsers.FastQ;
 public class CollectFasta
 {
 	public static final String FASTA_OUT = "/nobackup/afodor_research/datasets/chinaDec2017/MBiome/fastaFromRaw";
+	public static final String FASTQ_OUT = "/nobackup/afodor_research/datasets/chinaDec2017/MBiome/gatheredFastq";
 	
 	private static final String[] DIRS_TO_SCAN = 
 		{
@@ -75,15 +78,23 @@ public class CollectFasta
 		{
 			File forwardFile = findOneFile(f, f.getName(), 1);
 			
-			File outFasta1 = new File(FASTA_OUT + File.separator + f.getName() + "_1.fasta");
+			Files.copy(forwardFile.toPath(), 
+					new File( FASTQ_OUT + File.separator + f.getName() + "_1.fastq.gz" ).toPath());
 			
-			fastqToFasta(forwardFile, outFasta1);
+			//File outFasta1 = new File(FASTA_OUT + File.separator + f.getName() + "_1.fasta");
+			
+			//fastqToFasta(forwardFile, outFasta1);
 			
 			File backwardsFile = findOneFile(f, f.getName(), 2);
-		
-			File outFasta2 = new File(FASTA_OUT + File.separator + f.getName() + "_2.fasta");
 			
-			fastqToFasta(backwardsFile, outFasta2);
+
+			Files.copy(backwardsFile.toPath(), 
+					new File( FASTQ_OUT + File.separator + f.getName() + "_2.fastq.gz" ).toPath());
+			
+		
+			//File outFasta2 = new File(FASTA_OUT + File.separator + f.getName() + "_2.fasta");
+			
+			//fastqToFasta(backwardsFile, outFasta2);
 			x++;
 			System.out.println(x);
 			
