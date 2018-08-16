5642dceaca513443bdea5c69be20f21490e0656c
afodor <afodor@CCI7NR4PN2WWS.dyn.uncc.edu>
2018-08-16 15:47:24 -0400

    write fastaout for abundantOTU

---
+++ src/farnazAbundantOTU/MergeSequences.java
@@ -0,0 +1,42 @@
+package farnazAbundantOTU;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+
+import parsers.FastaSequence;
+import parsers.FastaSequenceOneAtATime;
+
+public class MergeSequences
+{
+	private static final String INPUT_DIRECTORY = "/scratch/afodor_research/datasets/GastricBypass/mbp_ready";
+	private static final String OUTPUT_DIRECTORY = "/scratch/afodor_research/datasets/GastricBypass/abundantOTU";
+	
+	public static void main(String[] args) throws Exception
+	{
+		BufferedWriter writer = new BufferedWriter(new FileWriter(new File(
+			OUTPUT_DIRECTORY + File.separator + "combinedFasta.txt"	)));
+		
+		String[] files = new File(INPUT_DIRECTORY).list();
+		
+		for(String s : files)
+			if( s.endsWith(".fasta.gz"))
+			{
+				String sampleName = s.replace(".fastq.gz", "");
+				
+				int index = 1;
+				File inFile = new File(INPUT_DIRECTORY + File.separator + s);
+				
+				FastaSequenceOneAtATime fsoat = new FastaSequenceOneAtATime(inFile,true);
+				
+				for(FastaSequence fs = fsoat.getNextSequence(); fs != null; fs =fsoat.getNextSequence())
+				{
+					writer.write(">" + sampleName + "_" + index  + "\n");
+					writer.write( fs.getSequence() + "\n");
+					index++;
+				}
+			}
+		
+		writer.flush();  writer.close();
+	}
+}
