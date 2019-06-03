90beead4dc9ebd9bdf7ff0a1ddf1ec1075358018
afodor <afodor@CCI7NR4PN2WWS.dyn.uncc.edu>
2019-06-03 16:03:03 -0400

    first test maybe

--- src/biolockJTestHardCoded/TestRDP.java
+++ src/biolockJTestHardCoded/TestRDP.java
@@ -19,15 +19,17 @@ public class TestRDP
 	
 	public static final int THRESHOLD = 80;
 	
+	public static final String LEVEL = "family";
+	
 	public static void main(String[] args) throws Exception
 	{
 		HashMap<String, HashMap<String,Long>> map = 
-				getCountsForDirectory(new File(IN_RDP_DIRECTORY), "family");
+				getCountsForDirectory(new File(IN_RDP_DIRECTORY), LEVEL);
 		
-		checkForDirectory(new File(BULK_RDP_DIRECTORY), map);
+		checkForDirectory(new File(BULK_RDP_DIRECTORY), map, LEVEL);
 	}
 	
-	private static void checkForDirectory(File inDir,HashMap<String, HashMap<String,Long>> map ) throws Exception
+	private static void checkForDirectory(File inDir,HashMap<String, HashMap<String,Long>> map, String level ) throws Exception
 	{
 		String[] files = inDir.list();
 		
@@ -45,15 +47,17 @@ public class TestRDP
 					throw new Exception("Could not find " + aName + " in " + map.keySet());
 				
 				HashMap<String,Long> innerMap = map.get(aName);
-				assertEqual(inFile, innerMap);
+				assertEqual(inFile, innerMap,level);
 			}
 		}
 	}
 	
-	private static void assertEqual( File inFile, HashMap<String,Long> innerMap ) throws Exception
+	private static void assertEqual( File inFile, HashMap<String,Long> innerMap, String level ) throws Exception
 	{
 		System.out.println("check " + inFile.getAbsolutePath());
 		BufferedReader reader = new BufferedReader(new FileReader(inFile));
+		
+		int checked =0;
 		
 		for(String s= reader.readLine(); s!= null; s= reader.readLine() )
 		{
@@ -61,9 +65,28 @@ public class TestRDP
 			
 			if( sToken.countTokens() != 2)
 				throw new Exception("Expecting two tokens");
+			
+			String name = sToken.nextToken();
+			
+			if( name.toLowerCase().indexOf("unclassified") == -1)
+			{
+				name = name.substring(name.indexOf(level + "__")+1, name.length());
+				
+				Long parsedVal = innerMap.get(name);
+				
+				if( parsedVal == null)
+					throw new Exception("Could not find " + name);
+				
+				Long thisVal = Long.parseLong(sToken.nextToken());
+				
+				if( ! thisVal.equals(parsedVal))
+					throw new Exception("Mismatched for " + name + " " + parsedVal + " " + thisVal);
+				
+				checked++;
+			}
 		}
 		
-		System.out.println("Ok");
+		System.out.println("Ok checked " + checked);
 	}
 	
 	
