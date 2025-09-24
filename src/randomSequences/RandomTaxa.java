f159642f9dc4562c6ae045231930a8977ff60426
Anthony Fodor <anthony.fodor@gmail.com>
2025-09-23 22:56:18 -0400

    initial commit

---
+++ src/randomSequences/RandomTaxa.java
@@ -0,0 +1,15 @@
+package randomSequences;
+
+public class RandomTaxa
+{
+	private static final String OUT_DIR = "C:\\temp";
+	
+	private static final int SEQUENCE_LENGTH = 150;
+	
+	private static final float FRACTION_DOMINANT_NUCLEOTIDE = 0.5f;
+	
+	public static void main(String[] args)
+	{
+		
+	}
+}
